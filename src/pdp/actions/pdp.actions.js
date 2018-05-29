4ca21f4eb0e9ec6796fe397a1f7a25debf2b5a96
InshaNazeer <insha@xelpmoc.in>
2018-05-29 18:05:53 +0530

    remove expected code

--- src/pdp/actions/pdp.actions.js
+++ src/pdp/actions/pdp.actions.js
@@ -312,7 +312,6 @@ export function addProductToCart(userId, cartId, accessToken, productDetails) {
       // here we dispatch a modal to show something was added to the bag
       dispatch(setBagCount(bagItemsInJsonFormat.length));
       dispatch(showModal(GO_TO_CART_PAGE_POPUP, productDetails));
-      // dispatch(displayToast("Added product to Bag"));
       setDataLayerForPdpDirectCalls(SET_DATA_LAYER_FOR_ADD_TO_BAG_EVENT);
       return dispatch(addProductToCartSuccess());
       // ADOBE_ADD_TO_CART
