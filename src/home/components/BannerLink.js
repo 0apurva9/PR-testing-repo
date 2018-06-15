e7c010fe12c17ad7c9cb559d274754bee7a3b28a
john2193 <janardan@xelpmoc.in>
2018-06-15 18:49:34 +0530

    banner and link

---
+++ src/home/components/BannerLink.js
@@ -0,0 +1,63 @@
+import React from "react";
+import styles from "./BannerLink.css";
+import Image from "../../xelpmoc-core/Image";
+import { TATA_CLIQ_ROOT } from "../../lib/apiRequest.js";
+import PropTypes from "prop-types";
+import UnderLinedButton from "../../general/components/UnderLinedButton";
+export default class BannerLink extends React.Component {
+  showSeeAll() {
+    if (this.props.showSeeAll) {
+      this.props.showSeeAll();
+    }
+  }
+  linkDetails = url => {
+    const urlSuffix = url.replace(TATA_CLIQ_ROOT, "$1");
+    this.props.history.push(urlSuffix);
+  };
+  redirectToLink() {
+    if (this.props.redirectToLink) {
+      this.props.redirectToLink();
+    }
+  }
+  render() {
+    return (
+      <div className={styles.base}>
+        <div className={styles.bannerImageHolder}>
+          <div
+            className={styles.imageHolder}
+            style={{
+              backgroundImage: `url(${this.props.image})`,
+              backgroundSize: "auto 80%",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center"
+            }}
+            onClick={() => this.redirectToLink()}
+          />
+        </div>
+        <div className={styles.linkDetailsHolder}>
+          <div className={styles.linkHeader}>{this.props.linkHeader}</div>
+          <div className={styles.linkDetails}>
+            {this.props.subItems &&
+              this.props.subItems.map((val, i) => {
+                return (
+                  <div
+                    className={styles.link}
+                    onClick={() => this.linkDetails(val.webURL)}
+                  >
+                    {val.title}
+                  </div>
+                );
+              })}
+          </div>
+          <div className={styles.allListButtonHolder}>
+            <UnderLinedButton
+              label="See all"
+              color="#212121"
+              onClick={() => this.showSeeAll()}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/home/components/ShopeByPriceDesktop.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Carousel from "../../general/components/Carousel";
+import BannerLink from "./BannerLink.js";
+import styles from "./ShopByPriceDesktop.css";
+import { TATA_CLIQ_ROOT } from "../../lib/apiRequest.js";
+export default class ShopeByPriceDesktop extends React.Component {
+  redirectToLink = url => {
+    const urlSuffix = url.replace(TATA_CLIQ_ROOT, "$1");
+    this.props.history.push(urlSuffix);
+  };
+  render() {
+    return (
+      <div className={styles.base}>
+        <Carousel header="Shope By Price" elementWidthDesktop={50}>
+          {this.props.items &&
+            this.props.items.map((datum, i) => {
+              return (
+                <BannerLink
+                  key={i}
+                  image={datum.imageURL}
+                  redirectToLink={() => this.redirectToLink(datum.webURL)}
+                  linkHeader={datum.title}
+                  subItems={datum.subItems}
+                  history={this.props.history}
+                />
+              );
+            })}
+        </Carousel>
+      </div>
+    );
+  }
+}
