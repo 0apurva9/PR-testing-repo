d15b5489a55b48a113ad7bc93410fac0fba74de5
InshaNazeer <insha@xelpmoc.in>
2018-06-27 11:03:11 +0530

    add conditions

--- src/home/components/SplitBannerForDesktop.js
+++ src/home/components/SplitBannerForDesktop.js
@@ -5,8 +5,22 @@ import { TATA_CLIQ_ROOT } from "../../lib/apiRequest.js";
 
 export default class SplitBannerForDesktop extends React.Component {
   handleClick(webURL) {
-    const urlSuffix = webURL.replace(TATA_CLIQ_ROOT, "$1");
-    this.props.history.push(urlSuffix);
+    if (webURL) {
+      const urlSuffix = webURL.replace(TATA_CLIQ_ROOT, "$1");
+      if (webURL.includes("/que")) {
+        window.open(urlSuffix, "_blank");
+        window.focus();
+      }
+      if (webURL.includes("/luxury.tatacliq.com")) {
+        window.open(webURL, "_blank");
+        window.focus();
+      } else {
+        this.props.history.push(urlSuffix);
+        if (this.props.setClickedElementId) {
+          this.props.setClickedElementId();
+        }
+      }
+    }
   }
 
   render() {
