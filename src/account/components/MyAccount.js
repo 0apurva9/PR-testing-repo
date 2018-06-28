c74e623474b270653ce11083b2443ffe4124079d
InshaNazeer <insha@xelpmoc.in>
2018-06-28 18:10:27 +0530

    makeAccountPageResponsive

--- src/account/components/MyAccount.js
+++ src/account/components/MyAccount.js
@@ -10,7 +10,10 @@ import AccountUsefulLink from "./AccountUsefulLink.js";
 import TabHolder from "./TabHolder";
 import TabData from "./TabData";
 import styles from "./MyAccount.css";
+import MediaQuery from "react-responsive";
 import LogoutButtonContainer from "../containers/LogoutButtonContainer";
+import PofileMenuGridForDesktop from "./PofileMenuGridForDesktop";
+import UserProfile from "./UserProfile";
 import {
   LOGGED_IN_USER_DETAILS,
   CUSTOMER_ACCESS_TOKEN,
@@ -77,128 +80,164 @@ export default class MyAccount extends React.Component {
     const userDetails = JSON.parse(userDetailsCookie);
     return (
       <div className={styles.base}>
-        <ProfileMenuGrid {...this.props} />
-        <div className={styles.accountHolder}>
-          <AccountSetting
-            image={userDetails.imageUrl}
-            onClick={() => this.renderToAccountSetting()}
-            firstName={
-              userDetails &&
-              userDetails.firstName &&
-              userDetails.firstName.trim().charAt(0)
-            }
-            heading={
-              userDetails &&
-              userDetails.firstName &&
-              `${userDetails.firstName} `
-            }
-            lastName={
-              userDetails && userDetails.lastName && `${userDetails.lastName}`
-            }
-          />
-          <div className={styles.logoutButton}>
-            <LogoutButtonContainer />
-          </div>
-        </div>
-        <div className={styles.tabHolder}>
-          <TabHolder>
-            <TabData
-              width="40%"
-              label="Recent Orders "
-              selected={this.state.isSelected === 0}
-              selectItem={() => this.tabSelect(0)}
-            />
-            <TabData
-              width="40%"
-              label="Useful Links "
-              selected={this.state.isSelected === 3}
-              selectItem={() => this.tabSelect(3)}
-            />
-            <TabData
-              width="40%"
-              label="Alerts "
-              selected={this.state.isSelected === 1}
-              selectItem={() => this.tabSelect(1)}
-            />
-            <TabData
-              width="40%"
-              label="Coupons "
-              selected={this.state.isSelected === 2}
-              selectItem={() => this.tabSelect(2)}
-            />
-          </TabHolder>
-        </div>
-        <div className={styles.dataHolder}>
-          {this.state.isSelected === 0 && (
-            <div className={styles.ordersHolder}>
-              <div className={styles.recentOrderHolder}>
-                <AllOrderContainer
-                  shouldCallHeaderContainer={false}
-                  shouldCallSetDataLayer={false}
-                />
-              </div>
-            </div>
-          )}
-          {this.state.isSelected === 3 && (
-            <div className={styles.useFulLinkHolder}>
-              <div className={styles.linkTabHolder}>
-                <a target="_blank" href="https://www.tatacliq.com/que">
-                  <AccountUsefulLink>
-                    <div className={styles.usefulLinkText}>Que Magazine</div>
-                  </AccountUsefulLink>
-                </a>
-                {/* <AccountUsefulLink>
-                  <div className={styles.usefulLinkText}>Our Stores</div>
-                </AccountUsefulLink> */}
-              </div>
-              <div className={styles.linkTabHolder}>
-                <AccountUsefulLink
-                  onClick={() => this.redirectToHelp(HELP_URL)}
-                >
-                  <div className={styles.usefulLinkText}>Help & Services</div>
-                </AccountUsefulLink>
-                <AccountUsefulLink
-                  onClick={() => this.redirectPage(PRIVACY_POLICY_URL)}
-                >
-                  <div className={styles.usefulLinkText}>Privacy policy</div>
-                </AccountUsefulLink>
-                <AccountUsefulLink>
-                  <div className={styles.usefulLinkText}>
-                    <div className={styles.callClass}>
-                      <a href="tel:9029108282">Call Tata CLIQ Care</a>
-                    </div>
-                  </div>
-                </AccountUsefulLink>
-                <AccountUsefulLink
-                  onClick={() => this.redirectPage(TERMS_AND_CONDITION_URL)}
-                >
-                  <div className={styles.usefulLinkText}>
-                    Terms & Conditions
-                  </div>
-                </AccountUsefulLink>
-                <AccountUsefulLink
-                  onClick={() => this.redirectPage(ABOUT_US_URL)}
-                >
-                  <div className={styles.usefulLinkText}>About us</div>
-                </AccountUsefulLink>
-                <AccountUsefulLink onClick={() => this.redirectPage(FAQ_URL)}>
-                  <div className={styles.usefulLinkText}>FAQ</div>
-                </AccountUsefulLink>
-              </div>
-            </div>
-          )}
-          {this.state.isSelected === 1 && (
-            <div className={styles.alertsHolder}>
-              <UserAlertsContainer />
-            </div>
-          )}
-          {this.state.isSelected === 2 && (
-            <div className={styles.couponHolder}>
-              <UserCouponsContainer
-                displayToast={message => this.props.displayToast(message)}
+        <div className={styles.holder}>
+          <MediaQuery query="(max-device-width: 1024px)">
+            <ProfileMenuGrid {...this.props} />
+            <div className={styles.accountHolder}>
+              <AccountSetting
+                image={userDetails.imageUrl}
+                onClick={() => this.renderToAccountSetting()}
+                firstName={
+                  userDetails &&
+                  userDetails.firstName &&
+                  userDetails.firstName.trim().charAt(0)
+                }
+                heading={
+                  userDetails &&
+                  userDetails.firstName &&
+                  `${userDetails.firstName} `
+                }
+                lastName={
+                  userDetails &&
+                  userDetails.lastName &&
+                  `${userDetails.lastName}`
+                }
               />
+              <div className={styles.logoutButton}>
+                <LogoutButtonContainer />
+              </div>
             </div>
-          )}
+          </MediaQuery>
+          <MediaQuery query="(min-device-width: 1025px)">
+            <PofileMenuGridForDesktop {...this.props} />
+          </MediaQuery>
+          <div className={styles.orderDetailsWithHolder}>
+            <div className={styles.tabHolder}>
+              <TabHolder>
+                <TabData
+                  width="40%"
+                  label="Recent Orders "
+                  selected={this.state.isSelected === 0}
+                  selectItem={() => this.tabSelect(0)}
+                />
+                <TabData
+                  width="40%"
+                  label="Useful Links "
+                  selected={this.state.isSelected === 3}
+                  selectItem={() => this.tabSelect(3)}
+                />
+                <TabData
+                  width="40%"
+                  label="Alerts "
+                  selected={this.state.isSelected === 1}
+                  selectItem={() => this.tabSelect(1)}
+                />
+                <TabData
+                  width="40%"
+                  label="Coupons "
+                  selected={this.state.isSelected === 2}
+                  selectItem={() => this.tabSelect(2)}
+                />
+              </TabHolder>
+            </div>
+
+            <div className={styles.dataHolder}>
+              {this.state.isSelected === 0 && (
+                <div className={styles.ordersHolder}>
+                  <div className={styles.recentOrderHolder}>
+                    <AllOrderContainer
+                      shouldCallHeaderContainer={false}
+                      shouldCallSetDataLayer={false}
+                    />
+                  </div>
+                </div>
+              )}
+              {this.state.isSelected === 3 && (
+                <div className={styles.useFulLinkHolder}>
+                  <div className={styles.linkTabHolder}>
+                    <a target="_blank" href="https://www.tatacliq.com/que">
+                      <AccountUsefulLink>
+                        <div className={styles.usefulLinkText}>
+                          Que Magazine
+                        </div>
+                      </AccountUsefulLink>
+                    </a>
+                  </div>
+                  <div className={styles.linkTabHolder}>
+                    <AccountUsefulLink
+                      onClick={() => this.redirectToHelp(HELP_URL)}
+                    >
+                      <div className={styles.usefulLinkText}>
+                        Help & Services
+                      </div>
+                    </AccountUsefulLink>
+                    <AccountUsefulLink
+                      onClick={() => this.redirectPage(PRIVACY_POLICY_URL)}
+                    >
+                      <div className={styles.usefulLinkText}>
+                        Privacy policy
+                      </div>
+                    </AccountUsefulLink>
+                    <AccountUsefulLink>
+                      <div className={styles.usefulLinkText}>
+                        <div className={styles.callClass}>
+                          <a href="tel:9029108282">Call Tata CLIQ Care</a>
+                        </div>
+                      </div>
+                    </AccountUsefulLink>
+                    <AccountUsefulLink
+                      onClick={() => this.redirectPage(TERMS_AND_CONDITION_URL)}
+                    >
+                      <div className={styles.usefulLinkText}>
+                        Terms & Conditions
+                      </div>
+                    </AccountUsefulLink>
+                    <AccountUsefulLink
+                      onClick={() => this.redirectPage(ABOUT_US_URL)}
+                    >
+                      <div className={styles.usefulLinkText}>About us</div>
+                    </AccountUsefulLink>
+                    <AccountUsefulLink
+                      onClick={() => this.redirectPage(FAQ_URL)}
+                    >
+                      <div className={styles.usefulLinkText}>FAQ</div>
+                    </AccountUsefulLink>
+                  </div>
+                </div>
+              )}
+              {this.state.isSelected === 1 && (
+                <div className={styles.alertsHolder}>
+                  <UserAlertsContainer />
+                </div>
+              )}
+              {this.state.isSelected === 2 && (
+                <div className={styles.couponHolder}>
+                  <UserCouponsContainer
+                    displayToast={message => this.props.displayToast(message)}
+                  />
+                </div>
+              )}
+            </div>
+          </div>
+          <MediaQuery query="(min-device-width: 1025px)">
+            <UserProfile
+              image={userDetails.imageUrl}
+              onClick={() => this.renderToAccountSetting()}
+              firstName={
+                userDetails &&
+                userDetails.firstName &&
+                userDetails.firstName.trim().charAt(0)
+              }
+              heading={
+                userDetails &&
+                userDetails.firstName &&
+                `${userDetails.firstName} `
+              }
+              lastName={
+                userDetails && userDetails.lastName && `${userDetails.lastName}`
+              }
+            />
+          </MediaQuery>
         </div>
       </div>
     );
---
+++ src/account/components/PofileMenuGridForDesktop.js
@@ -0,0 +1,98 @@
+import React from "react";
+import styles from "./PofileMenuGridForDesktop.css";
+import {
+  MY_ACCOUNT_PAGE,
+  MY_ACCOUNT_ORDERS_PAGE,
+  MY_ACCOUNT_GIFT_CARD_PAGE,
+  MY_ACCOUNT_SAVED_CARDS_PAGE,
+  MY_ACCOUNT_ADDRESS_PAGE,
+  MY_ACCOUNT_BRANDS_PAGE,
+  MY_ACCOUNT_UPDATE_PROFILE_PAGE,
+  MY_ACCOUNT_ALERTS_PAGE,
+  MY_ACCOUNT_CLIQ_CASH_PAGE,
+  SAVE_LIST_PAGE
+} from "../../lib/constants";
+import savedList from "../../general/components/img/Save.svg";
+import addressBook from "./img/addressbook.svg";
+import brands from "./img/brand.svg";
+import orderHistory from "../../general/components/img/order-history.svg";
+import savedPayments from "./img/card.svg";
+import alertsCoupons from "../../general/components/img/notification.svg";
+import cliqCash from "./img/cliqCash.svg";
+import giftCards from "./img/giftCards.svg";
+import settings from "./img/settings.svg";
+import Icon from "../../xelpmoc-core/Icon";
+import PropTypes from "prop-types";
+export default class PofileMenuGridForDesktop extends React.Component {
+  onSave(value) {
+    if (value) {
+      this.props.history.push(`${MY_ACCOUNT_PAGE}${value}`);
+    }
+  }
+  render() {
+    const data = [
+      {
+        image: savedList,
+        text: "Saved List",
+        url: SAVE_LIST_PAGE
+      },
+      {
+        image: orderHistory,
+        text: "Order History",
+        url: MY_ACCOUNT_ORDERS_PAGE
+      },
+      {
+        image: addressBook,
+        text: "Address Book",
+        url: MY_ACCOUNT_ADDRESS_PAGE
+      },
+      { image: brands, text: "Brands", url: MY_ACCOUNT_BRANDS_PAGE },
+
+      {
+        image: savedPayments,
+        text: "Saved Payments",
+        url: MY_ACCOUNT_SAVED_CARDS_PAGE
+      },
+      {
+        image: alertsCoupons,
+        text: "Alerts & Coupons",
+        url: MY_ACCOUNT_ALERTS_PAGE
+      },
+      { image: giftCards, text: "Gift Card", url: MY_ACCOUNT_GIFT_CARD_PAGE },
+      { image: cliqCash, text: "CLiQ Cash", url: MY_ACCOUNT_CLIQ_CASH_PAGE },
+      { image: settings, text: "Settings", url: MY_ACCOUNT_UPDATE_PROFILE_PAGE }
+    ];
+
+    return (
+      <div className={styles.base}>
+        {data.map((datum, i) => {
+          return (
+            <div
+              className={styles.holder}
+              onClick={value => this.onSave(datum.url)}
+            >
+              <div className={styles.gridLogo}>
+                <Icon image={datum.image} size={20} />
+              </div>
+              <div className={styles.gridText}>{datum.text}</div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+PofileMenuGridForDesktop.propTypes = {
+  onClick: PropTypes.func,
+  title: PropTypes.string,
+  btnText: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  logo: PropTypes.string,
+  image: PropTypes.string,
+  header: PropTypes.string,
+  color: PropTypes.string
+};
+PofileMenuGridForDesktop.defaultProps = {
+  color: "#fff",
+  backgroundColor: "#ff1744"
+};
