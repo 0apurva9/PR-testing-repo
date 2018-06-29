c7a216eeee7ba07f5b63958c51a175e992040f3c
ShitanshuRoy <shitanshuroy@gmail.com>
2018-06-29 16:57:02 +0530

    added some changes

--- src/cart/components/OrderConfirmation.js
+++ src/cart/components/OrderConfirmation.js
@@ -4,6 +4,7 @@ import OrderBanner from "./OrderBanner.js";
 import styles from "./OrderConfirmation.css";
 import OrderDetailsCard from "./OrderDetailsCard.js";
 import OrderConfirmationFooter from "./OrderConfirmationFooter.js";
+import MediaQuery from "react-responsive";
 export default class OrderConfirmation extends React.Component {
   captureOrderExperience = rating => {
     this.props.captureOrderExperience(rating);
@@ -19,39 +20,54 @@ export default class OrderConfirmation extends React.Component {
   render() {
     return (
       <div className={styles.base}>
-        <div className={styles.orderBannerHolder}>
-          <OrderBanner
-            headingText={this.props.orderStatusMessage}
-            label={this.props.orderId}
-            onClick={() => this.trackOrder()}
-          />
-        </div>
-        <div className={styles.rateHolder}>
-          <RateYourExperienceCard
-            captureOrderExperience={rating =>
-              this.captureOrderExperience(rating)
-            }
-            continueShopping={() => this.continueShopping()}
-          />
-        </div>
-        {this.props.orderDetails &&
-          this.props.orderDetails.products &&
-          this.props.orderDetails.products.map(order => {
-            return (
-              <OrderDetailsCard
-                productDetails={order}
-                orderDetails={this.props.orderDetails}
-                orderId={this.props.orderId}
+        <div className={styles.pageCenter}>
+          <div className={styles.leftSection}>
+            <div className={styles.orderBannerHolder}>
+              <OrderBanner
+                headingText={this.props.orderStatusMessage}
+                label={this.props.orderId}
+                onClick={() => this.trackOrder()}
+              />
+            </div>
+            <div className={styles.rateHolder}>
+              <RateYourExperienceCard
+                captureOrderExperience={rating =>
+                  this.captureOrderExperience(rating)
+                }
+                continueShopping={() => this.continueShopping()}
+              />
+            </div>
+            {this.props.orderDetails &&
+              this.props.orderDetails.products &&
+              this.props.orderDetails.products.map(order => {
+                return (
+                  <OrderDetailsCard
+                    productDetails={order}
+                    orderDetails={this.props.orderDetails}
+                    orderId={this.props.orderId}
+                    trackOrder={() => this.trackOrder()}
+                  />
+                );
+              })}
+            <MediaQuery query="(min-device-width: 1025px)">
+              <OrderConfirmationFooter
+                isEgvOrder={this.props.orderDetails.isEgvOrder}
+                continueShopping={() => this.continueShopping()}
                 trackOrder={() => this.trackOrder()}
               />
-            );
-          })}
-
-        <OrderConfirmationFooter
-          isEgvOrder={this.props.orderDetails.isEgvOrder}
-          continueShopping={() => this.continueShopping()}
-          trackOrder={() => this.trackOrder()}
-        />
+            </MediaQuery>
+          </div>
+          <MediaQuery query="(max-device-width: 1024px)">
+            <div className={styles.rightSection}>
+              <RateYourExperienceCard
+                captureOrderExperience={rating =>
+                  this.captureOrderExperience(rating)
+                }
+                continueShopping={() => this.continueShopping()}
+              />
+            </div>
+          </MediaQuery>
+        </div>
       </div>
     );
   }
