dec4e0880f05e507a52802973b248fb7f1bf0147
ShitanshuRoy <shitanshuroy@gmail.com>
2018-07-02 16:02:51 +0530

    added propTypes

--- src/home/components/MultiClickBanner.js
+++ src/home/components/MultiClickBanner.js
@@ -1,54 +1,9 @@
 import React from "react";
 import MultiClickProduct from "./MultiClickProduct";
 import { TATA_CLIQ_ROOT } from "../../lib/apiRequest.js";
+import PropTypes from "prop-types";
 import styles from "./MultiClickBanner.css";
-const data = [
-  {
-    backgroundImage:
-      "https://www.appliancesonline.com.au/public/images/product/eqe6207sd/external/620L-Electrolux-4-Door-Fridge-EQE6207SD-Hero-Image-high.jpeg",
-    yAxis: 50,
-    xAxis: 100,
-    brandName: "Philips",
-    description: "Fridge HD7447/20",
-    price: "2,400",
-    url:
-      "https://www.tatacliq.com/samsung-rh62k6007s8-674l-refrigerator-elegant-inox/p-mp000000001641288"
-  },
-  {
-    backgroundImage:
-      "http://storage.torontosun.com/v1/blogs-prod-photos/8/2/d/d/7/82dd762da57fa9de6d492c0c5221eb3f.jpg",
-    yAxis: 30,
-    xAxis: 5,
-    brandName: "Philips",
-    description: "Coffee maker HD7447/20",
-    price: "2,400",
-    url:
-      "https://www.tatacliq.com/samsung-rh62k6007s8-674l-refrigerator-elegant-inox/p-mp000000001641288"
-  },
-  {
-    backgroundImage:
-      "https://images.philips.com/is/image/PhilipsConsumer/HD7431_20-IMS-en_IN?$jpglarge$&wid=1250",
-    yAxis: 0,
-    xAxis: 100,
-    brandName: "Philips",
-    description: "Toaster HD7447/20",
-    price: "2,400",
-    url:
-      "https://www.tatacliq.com/samsung-rh62k6007s8-674l-refrigerator-elegant-inox/p-mp000000001641288"
-  },
-  {
-    backgroundImage:
-      "http://storage.torontosun.com/v1/blogs-prod-photos/8/2/d/d/7/82dd762da57fa9de6d492c0c5221eb3f.jpg",
-    yAxis: 47.5,
-    xAxis: 94,
-    brandName: "Philips",
-    description: "Coffee maker HD7447/20",
-    price: "2,400",
 
-    url:
-      "https://www.tatacliq.com/samsung-rh62k6007s8-674l-refrigerator-elegant-inox/p-mp000000001641288"
-  }
-];
 export default class MultiClickBanner extends React.Component {
   goToUrl(url) {
     const urlSuffix = url.replace(TATA_CLIQ_ROOT, "$1");
@@ -67,41 +22,65 @@ export default class MultiClickBanner extends React.Component {
     }
   }
   render() {
+    const data = this.props.data;
     return (
       <div
         className={
           this.props.positionInFeed === 1 ? styles.firstItemBase : styles.base
         }
+        style={{
+          backgroundColor: data.backgroundColor ? data.backgroundColor : "",
+          backgroundImage: data.backgroundImage
+            ? `url(${data.backgroundImage})`
+            : ""
+        }}
       >
         <div className={styles.content}>
-          {data.map(val => {
-            return (
-              <div
-                className={styles.section}
-                style={{
-                  backgroundColor: val.backgroundColor,
-                  backgroundImage: `url(${val.backgroundImage})`
-                }}
-              >
+          {data &&
+            data.items &&
+            data.items.map(val => {
+              return (
                 <div
-                  className={styles.details}
+                  className={styles.section}
                   style={{
-                    top: `${val.yAxis}%`,
-                    left: `${val.xAxis}%`
+                    backgroundColor: val.backgroundColor,
+                    backgroundImage: `url(${val.backgroundImage})`
                   }}
                 >
-                  <MultiClickProduct
-                    brandName={val.brandName}
-                    description={val.description}
-                    price={val.price}
-                    onClick={() => this.goToUrl(val.url)}
-                  />
+                  <div
+                    className={styles.details}
+                    style={{
+                      top: `${val.yAxis}%`,
+                      left: `${val.xAxis}%`
+                    }}
+                  >
+                    <MultiClickProduct
+                      brandName={val.brandName}
+                      description={val.description}
+                      price={val.price}
+                      onClick={() => this.goToUrl(val.url)}
+                    />
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
         </div>
       </div>
     );
   }
 }
+MultiClickBanner.propTypes = {
+  backgroundColor: PropTypes.string,
+  backgroundImage: PropTypes.string,
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      backgroundImage: PropTypes.string,
+      backgroundColor: PropTypes.string,
+      yAxis: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+      xAxis: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+      brandName: PropTypes.string,
+      description: PropTypes.string,
+      price: PropTypes.string
+    })
+  )
+};
--- src/home/components/MultiClickProduct.js
+++ src/home/components/MultiClickProduct.js
@@ -1,7 +1,6 @@
 import React from "react";
-
 import styles from "./MultiClickProduct.css";
-
+import PropTypes from "prop-types";
 export default class MultiClickProduct extends React.Component {
   handleClick = () => {
     if (this.props.onClick) {
@@ -14,11 +13,18 @@ export default class MultiClickProduct extends React.Component {
         <div className={styles.background} />
         <div className={styles.icon} />
         <div className={styles.content}>
-          <div className={styles.bold}>Philips</div>
-          <div className={styles.light}>Coffee maker HD7447/20</div>
-          <div className={styles.light}>Rs. 2,400</div>
+          <div className={styles.bold}>{this.props.brandName}</div>
+          <div className={styles.light}>{this.props.description}</div>
+          <div className={styles.light}>{this.props.price}</div>
         </div>
       </div>
     );
   }
 }
+
+MultiClickProduct.propTypes = {
+  brandName: PropTypes.string,
+  description: PropTypes.string,
+  price: PropTypes.string,
+  onClick: PropTypes.func
+};
