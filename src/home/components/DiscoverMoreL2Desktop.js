d2d0ecd1b8e1dbd6aefd59f75c687aef74698143
john2193 <janardan@xelpmoc.in>
2018-07-03 15:19:09 +0530

    css fixing

--- src/home/components/DiscoverMoreL2Desktop.js
+++ src/home/components/DiscoverMoreL2Desktop.js
@@ -2,33 +2,35 @@ import React from "react";
 import Carousel from "../../general/components/Carousel";
 import DiscoverMoreComponentDesktop from "./DiscoverMoreComponentDesktop";
 import styles from "./DiscoverMoreL2Desktop.css";
+import DesktopOnly from "../../general/components/DesktopOnly";
 import PropTypes from "prop-types";
 export default class DiscoverMoreL2Desktop extends React.Component {
   render() {
     const { feedComponentData, title } = this.props;
-
     return (
-      <div className={styles.base}>
-        <Carousel
-          header={feedComponentData && feedComponentData.title}
-          elementWidthDesktop={33.33}
-        >
-          {feedComponentData.items &&
-            feedComponentData.items.map((datum, i) => {
-              return (
-                <DiscoverMoreComponentDesktop
-                  imageURL={datum.imageURL}
-                  title={datum.title}
-                  webURL={datum.webURL}
-                  btnText={datum.btnText}
-                  items={datum.items}
-                  history={this.props.history}
-                  setClickedElementId={this.props.setClickedElementId}
-                />
-              );
-            })}
-        </Carousel>
-      </div>
+      <DesktopOnly>
+        <div className={styles.base}>
+          <Carousel
+            header={feedComponentData && feedComponentData.title}
+            elementWidthDesktop={33.33}
+          >
+            {feedComponentData.items &&
+              feedComponentData.items.map((datum, i) => {
+                return (
+                  <DiscoverMoreComponentDesktop
+                    imageURL={datum.imageURL}
+                    title={datum.title}
+                    webURL={datum.webURL}
+                    btnText={datum.btnText}
+                    items={datum.items}
+                    history={this.props.history}
+                    setClickedElementId={this.props.setClickedElementId}
+                  />
+                );
+              })}
+          </Carousel>
+        </div>
+      </DesktopOnly>
     );
   }
 }
