928ae2bdbae8cb00d0275461c2beeeec584ca373
InshaNazeer <insha@xelpmoc.in>
2018-07-05 12:04:27 +0530

    makeCenterModal

---
+++ src/general/components/CenterModal.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./CenterModal.css";
+export default class CenterModal extends React.Component {
+  handleClose() {
+    if (this.props.closeModal) {
+      this.props.closeModal();
+    }
+  }
+  render() {
+    return (
+      <div className={styles.base}>
+        <div className={styles.content}>{this.props.children}</div>
+      </div>
+    );
+  }
+}
