43aeeb3680d7602347f07bdca91f88c37c92662c
Basavannavva <Basavannevva@xelpmoc.in>
2018-07-11 18:53:02 +0530

    Did chages

--- src/account/containers/ReturnToStoreContainer.js
+++ src/account/containers/ReturnToStoreContainer.js
@@ -40,7 +40,6 @@ const mapDispatchToProps = (dispatch, ownProps) => {
 };
 
 const mapStateToProps = (state, ownProps) => {
-  console.log(ownProps);
   return {
     ...ownProps,
     orderDetails: state.profile.fetchOrderDetails
