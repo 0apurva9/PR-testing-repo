3d0991a4a285a6ea263bad3367ea3cf1d524edb6
InshaNazeer <insha@xelpmoc.in>
2018-07-21 12:36:04 +0530

    make APlusTempate2

---
+++ src/pdp/components/APlusTemplate2.js
@@ -0,0 +1,123 @@
+import React from "react";
+import styles from "./APlusTemplates.css";
+export default class APlusTemplate1 extends React.Component {
+  render() {
+    let data = this.props.data;
+    return (
+      <div className={styles.base}>
+        {data["Section1"] && (
+          <div className={styles.section}>
+            <div className={styles.shortSection}>
+              {data["Section1"] &&
+              data["Section1"][0] &&
+              data["Section1"][0].value &&
+              data["Section1"][0].value.imageList &&
+              data["Section1"][0].value.imageList[0] ? (
+                <img src={data["Section1"][0].value.imageList[0]} alt="" />
+              ) : null}
+            </div>
+            <div className={styles.longSectionWithRightHeading}>
+              {data["Section1"] &&
+              data["Section1"][1] &&
+              data["Section1"][1].value &&
+              data["Section1"][1].value.textList &&
+              data["Section1"][1].value.textList[0] ? (
+                <div
+                  className={styles.textSection}
+                  dangerouslySetInnerHTML={{
+                    __html: data["Section1"][1].value.textList[0]
+                  }}
+                  alt=""
+                />
+              ) : null}
+            </div>
+          </div>
+        )}
+        {data["Section2"] && (
+          <div className={styles.section}>
+            <div className={styles.longSectionWithRightHeading}>
+              {data["Section2"] &&
+              data["Section2"][0] &&
+              data["Section2"][0].value &&
+              data["Section2"][0].value.textList &&
+              data["Section2"][0].value.textList[0] ? (
+                <div
+                  className={styles.textSection}
+                  dangerouslySetInnerHTML={{
+                    __html: data["Section2"][0].value.textList[0]
+                  }}
+                  alt=""
+                />
+              ) : null}
+            </div>
+            <div className={styles.shortSection}>
+              {data["Section2"] &&
+              data["Section2"][1] &&
+              data["Section2"][1].value &&
+              data["Section2"][1].value.imageList &&
+              data["Section2"][1].value.imageList[0] ? (
+                <img src={data["Section2"][1].value.imageList[0]} alt="" />
+              ) : null}
+            </div>
+          </div>
+        )}
+        {data["Section3"] && (
+          <div className={styles.section}>
+            <div className={styles.shortSection}>
+              {data["Section3"] &&
+              data["Section3"][0] &&
+              data["Section3"][0].value &&
+              data["Section3"][0].value.imageList &&
+              data["Section3"][0].value.imageList[0] ? (
+                <img src={data["Section3"][0].value.imageList[0]} alt="" />
+              ) : null}
+            </div>
+            <div className={styles.longSectionWithRightHeading}>
+              {data["Section3"] &&
+              data["Section3"][1] &&
+              data["Section3"][1].value &&
+              data["Section3"][1].value.textList &&
+              data["Section3"][1].value.textList[0] ? (
+                <div
+                  className={styles.textSection}
+                  dangerouslySetInnerHTML={{
+                    __html: data["Section3"][1].value.textList[0]
+                  }}
+                  alt=""
+                />
+              ) : null}
+            </div>
+          </div>
+        )}
+        {data["Section4"] && (
+          <div className={styles.section}>
+            <div className={styles.longSectionWithRightHeading}>
+              {data["Section4"] &&
+              data["Section4"][0] &&
+              data["Section4"][0].value &&
+              data["Section4"][0].value.textList &&
+              data["Section4"][0].value.textList[0] ? (
+                <div
+                  className={styles.textSection}
+                  dangerouslySetInnerHTML={{
+                    __html: data["Section4"][0].value.textList[0]
+                  }}
+                  alt=""
+                />
+              ) : null}
+            </div>
+            <div className={styles.shortSection}>
+              {data["Section4"] &&
+              data["Section4"][1] &&
+              data["Section4"][1].value &&
+              data["Section4"][1].value.imageList &&
+              data["Section4"][1].value.imageList[0] ? (
+                <img src={data["Section4"][1].value.imageList[0]} alt="" />
+              ) : null}
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
