e123d9ef2cb2aee67e289a98622fd285a3bd7964
srane15 <srane@tataunistore.com>
2018-08-01 16:22:47 +0530

    commented amp homepage routing code

--- index.js
+++ index.js
@@ -60,26 +60,27 @@ app.get("/*", (req, res) => {
   const origUrl = req.originalUrl;
 
   //Code start for AMP
-  if (origUrl.search("/amp/home") !== -1) {
-    var metaKeywords = "Tatacliq, Online Shopping, Online Shopping India";
-    var metaDescription =
-      "Online Shopping Site in India - Upto 60% Off On Mobiles, Electronics & Fashion at Tata CLiQ";
-    var canonicalHomeAmpUrl = req.protocol + "://" + req.get("host") + origUrl;
-    var canonicalHomePwaUrl = req.protocol + "://" + req.get("host");
-    var defaultPageUrl =
-      ampServicesStartPoint +
-      "/marketplacewebservices/v2/mpl/cms/defaultpage?pageId=defaulthomepage";
+  // if (origUrl.search("/amp/home") !== -1) {
+  //   var metaKeywords = "Tatacliq, Online Shopping, Online Shopping India";
+  //   var metaDescription =
+  //     "Online Shopping Site in India - Upto 60% Off On Mobiles, Electronics & Fashion at Tata CLiQ";
+  //   var canonicalHomeAmpUrl = req.protocol + "://" + req.get("host") + origUrl;
+  //   var canonicalHomePwaUrl = req.protocol + "://" + req.get("host");
+  //   var defaultPageUrl =
+  //     ampServicesStartPoint +
+  //     "/marketplacewebservices/v2/mpl/cms/defaultpage?pageId=defaulthomepage";
 
-    var data = {
-      metaKeywords: metaKeywords,
-      metaDescription: metaDescription,
-      canonicalAmpUrl: canonicalHomeAmpUrl,
-      canonicalPwaUrl: canonicalHomePwaUrl,
-      defaultPageUrl: defaultPageUrl
-    };
+  //   var data = {
+  //     metaKeywords: metaKeywords,
+  //     metaDescription: metaDescription,
+  //     canonicalAmpUrl: canonicalHomeAmpUrl,
+  //     canonicalPwaUrl: canonicalHomePwaUrl,
+  //     defaultPageUrl: defaultPageUrl
+  //   };
 
-    res.render("../build/amp/home.ejs", data);
-  } else if (origUrl.search("/amp/") !== -1 && origUrl.search("/p-") !== -1) {
+  //   res.render("../build/amp/home.ejs", data);
+  // } else
+  if (origUrl.search("/amp/") !== -1 && origUrl.search("/p-") !== -1) {
     var productCode = origUrl.split("p-")[1];
     var productDataAPI = "/src/product.json";
     var canonicalAmpUrl = req.protocol + "://" + req.get("host") + origUrl;
