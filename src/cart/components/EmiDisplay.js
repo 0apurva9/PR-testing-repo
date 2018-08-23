39c8d4c0c3db89fdab32d3ffc585f9ba4400ad2e
InshaNazeer <insha@xelpmoc.in>
2018-08-23 17:21:00 +0530

    chjangeEmi

--- src/cart/components/EmiDisplay.js
+++ src/cart/components/EmiDisplay.js
@@ -4,6 +4,9 @@ import Image from "../../xelpmoc-core/Image";
 import UnderLinedButton from "../../general/components/UnderLinedButton";
 import image from "../../general/components/img/check.svg";
 import PropTypes from "prop-types";
+import MobileOnly from "../../general/components/MobileOnly";
+import DesktopOnly from "../../general/components/DesktopOnly";
+import Button from "../../general/components/Button.js";
 export default class EmiDisplay extends React.Component {
   handleClick() {
     if (this.props.changePlan) {
@@ -19,22 +22,40 @@ export default class EmiDisplay extends React.Component {
       <div className={styles.base}>
         <div className={styles.header}>
           {this.props.bankName}
-          <div className={styles.button}>
-            <UnderLinedButton
-              label="Change plan"
-              onClick={() => {
-                this.handleClick();
-              }}
-            />
-          </div>
+          <MobileOnly>
+            <div className={styles.button}>
+              <UnderLinedButton
+                label="Change plan"
+                onClick={() => {
+                  this.handleClick();
+                }}
+              />
+            </div>
+          </MobileOnly>
         </div>
         <div className={styles.emiText}>
           {this.props.term} EMI @ {emiRate}
-          <span className={styles.price}>{this.props.price}</span>/Month
+          <span className={styles.price}>Rs. {this.props.price}</span>/Month
         </div>
-        <div className={styles.checkBoxHolder}>
-          <Image image={image} />
-        </div>
+        <DesktopOnly>
+          <div className={styles.buttonHolder}>
+            <Button
+              borderRadius={18}
+              borderColor="#d2d2d2"
+              backgroundColor="#f9f9f9"
+              height={36}
+              label={"Change plan"}
+              width={147}
+              textStyle={{ color: "#d2d2d2", fontSize: 14 }}
+              onClick={() => this.handleClick()}
+            />
+          </div>
+        </DesktopOnly>
+        <MobileOnly>
+          <div className={styles.checkBoxHolder}>
+            <Image image={image} />
+          </div>
+        </MobileOnly>
       </div>
     );
   }
