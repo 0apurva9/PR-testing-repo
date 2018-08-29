7e413dd7e4071ec1401cc98238159b4ad50fe2b2
InshaNazeer <insha@xelpmoc.in>
2018-08-29 14:46:02 +0530

    add search with functionality

--- src/plp/components/ShowBrandModal.js
+++ src/plp/components/ShowBrandModal.js
@@ -1,29 +1,53 @@
 import React from "react";
 import styles from "./ShowBrandModal.css";
+import SearchInput from "../../general/components/SearchInput";
 import CheckBox from "../../general/components/CheckBox.js";
 export default class ShowBrandModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brandSearchString: ""
+    };
+  }
   closeModal() {
     if (this.props.closeModal) {
       this.props.closeModal();
     }
   }
+  onBrandSearch = val => {
+    this.setState({ brandSearchString: val });
+  };
   onFilterClick = (val, evt) => {
     evt.preventDefault();
     const url = val.replace("{pageNo}", 1);
     this.props.history.push(url);
   };
   render() {
+    let brandsList = this.props.brandData;
+    if (this.state.brandSearchString !== "") {
+      brandsList = brandsList.filter(brand => {
+        return brand.name
+          .toLowerCase()
+          .includes(this.state.brandSearchString.toLowerCase());
+      });
+    }
     return (
       <div className={styles.base}>
         <div className={styles.headerElement}>
+          <div className={styles.search}>
+            <SearchInput
+              placeholder="Search for Brands"
+              onChange={val => this.onBrandSearch(val)}
+            />
+          </div>
           <div
             className={styles.crossElement}
             onClick={() => this.closeModal()}
           />
         </div>
         <div className={styles.displayDataElement}>
-          {this.props &&
-            this.props.brandData.map((val, i) => {
+          {brandsList &&
+            brandsList.map((val, i) => {
               return (
                 <div className={styles.brandNameHolder}>
                   <div
