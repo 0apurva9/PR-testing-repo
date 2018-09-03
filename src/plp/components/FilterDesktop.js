aafdb1d803ebe63e4f9d38a44caa2149b99a50ed
john2193 <janardan@xelpmoc.in>
2018-09-03 10:38:55 +0530

    design modal

--- src/plp/components/FilterDesktop.js
+++ src/plp/components/FilterDesktop.js
@@ -216,7 +216,7 @@ export default class FilterDesktop extends React.Component {
                                 );
                               })}
                           </div>
-                          <div className={styles.filterPriceHolder}>
+                          <div className={styles.filterBrandHolder}>
                             {facetDataValues &&
                               facetDataValues.name === BRAND &&
                               facetDataValues.values && (
