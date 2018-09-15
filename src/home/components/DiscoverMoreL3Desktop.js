af77a957eaab8edcbdfbc19e158089ba911cce8b
john2193 <janardan@xelpmoc.in>
2018-09-15 11:42:10 +0530

    remove hover effect

--- src/home/components/DiscoverMoreL3Desktop.js
+++ src/home/components/DiscoverMoreL3Desktop.js
@@ -2,6 +2,7 @@ import React from "react";
 import Carousel from "../../general/components/Carousel";
 import DiscoverMoreComponentDesktop from "./DiscoverMoreComponentDesktop";
 import styles from "./DiscoverMoreL3Desktop.css";
+import CommonCenter from "../../general/components/CommonCenter.js";
 import DesktopOnly from "../../general/components/DesktopOnly";
 import PropTypes from "prop-types";
 export default class DiscoverMoreL3Desktop extends React.Component {
@@ -9,27 +10,29 @@ export default class DiscoverMoreL3Desktop extends React.Component {
     const { feedComponentData, title } = this.props;
     return (
       <DesktopOnly>
-        <div className={styles.base}>
-          <Carousel
-            header={feedComponentData && feedComponentData.title}
-            elementWidthDesktop={33.33}
-          >
-            {feedComponentData.data &&
-              feedComponentData.data.map((datum, i) => {
-                return (
-                  <DiscoverMoreComponentDesktop
-                    imageURL={datum.imageURL}
-                    title={datum.title}
-                    webURL={datum.webURL}
-                    btnText={datum.btnText}
-                    items={datum.items}
-                    history={this.props.history}
-                    setClickedElementId={this.props.setClickedElementId}
-                  />
-                );
-              })}
-          </Carousel>
-        </div>
+        <CommonCenter>
+          <div className={styles.base}>
+            <Carousel
+              header={feedComponentData && feedComponentData.title}
+              elementWidthDesktop={33.33}
+            >
+              {feedComponentData.data &&
+                feedComponentData.data.map((datum, i) => {
+                  return (
+                    <DiscoverMoreComponentDesktop
+                      imageURL={datum.imageURL}
+                      title={datum.title}
+                      webURL={datum.webURL}
+                      btnText={datum.btnText}
+                      items={datum.items}
+                      history={this.props.history}
+                      setClickedElementId={this.props.setClickedElementId}
+                    />
+                  );
+                })}
+            </Carousel>
+          </div>
+        </CommonCenter>
       </DesktopOnly>
     );
   }
