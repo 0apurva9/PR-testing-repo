f1a97e6b6110e6b2db794dd2c032ec1369a8d101
InshaNazeer <insha@xelpmoc.in>
2018-10-11 12:27:37 +0530

    Add review section

--- src/account/components/UserReview.js
+++ src/account/components/UserReview.js
@@ -8,6 +8,10 @@ import {
   CUSTOMER_ACCESS_TOKEN
 } from "../../lib/constants";
 import Loader from "../../general/components/Loader";
+import DesktopOnly from "../../general/components/DesktopOnly";
+import MobileOnly from "../../general/components/MobileOnly";
+import OrderCard from "./OrderCard.js";
+import StarRating from "../../general/components/StarRating.js";
 export default class UserReview extends React.Component {
   componentDidMount() {
     if (this.props.getUserReview) {
@@ -64,26 +68,47 @@ export default class UserReview extends React.Component {
         this.props.userReview &&
         this.props.userReview.reviews ? (
           <React.Fragment>
-            <div className={styles.totalReview}>
-              You have made{" "}
-              <span className={styles.totalReviewLength}>
-                {this.props.userReview.reviews.length} reviews
-              </span>
-            </div>
+            <MobileOnly>
+              <div className={styles.totalReview}>
+                You have made{" "}
+                <span className={styles.totalReviewLength}>
+                  {this.props.userReview.reviews.length} reviews
+                </span>
+              </div>
+            </MobileOnly>
             {this.props.userReview.reviews.map((reviews, i) => {
               return (
                 <div className={styles.reviewsHolder} key={i}>
                   <div className={styles.productDetails}>
-                    <ProductDetailsCard
-                      productImage={reviews.productImageUrl}
-                      brandName={reviews.brandName}
-                      productTitle={reviews.productTitle}
-                      averageRating={reviews.rating}
-                      showAverageRatingWithDays={true}
-                      daysAgo={reviews.reviewAge}
-                    />
+                    <MobileOnly>
+                      <ProductDetailsCard
+                        productImage={reviews.productImageUrl}
+                        brandName={reviews.brandName}
+                        productTitle={reviews.productTitle}
+                        averageRating={reviews.rating}
+                        showAverageRatingWithDays={true}
+                        daysAgo={reviews.reviewAge}
+                      />
+                    </MobileOnly>
+                    <DesktopOnly>
+                      <OrderCard
+                        imageUrl={reviews.productImageUrl}
+                        productName={reviews.productTitle}
+                        productBrand={reviews.brandName}
+                        showIsGiveAway={false}
+                      />
+                    </DesktopOnly>
                   </div>
                   <div className={styles.commentAndHeadlineHolder}>
+                    {reviews.rating && (
+                      <div className={styles.rating}>
+                        <StarRating averageRating={reviews.rating}>
+                          <span className={styles.showAverageRatingWithDays}>
+                            {reviews.reviewAge}
+                          </span>
+                        </StarRating>
+                      </div>
+                    )}
                     <div className={styles.headline}>{reviews.headline} </div>
                     <div className={styles.comment}>{reviews.comment} </div>
                   </div>
