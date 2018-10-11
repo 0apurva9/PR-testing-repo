e9a47a2beb8fbd4caafa4f03c5d43d078e92b241
InshaNazeer <insha@xelpmoc.in>
2018-10-11 11:14:50 +0530

    revert code

--- src/pdp/components/PDPRecommendedSections.js
+++ src/pdp/components/PDPRecommendedSections.js
@@ -147,7 +147,7 @@ class PDPRecommendedSections extends React.Component {
   renderCarousel(items, elementWidthDesktop, widgetName) {
     return (
       <div className={styles.brandProductCarousel}>
-        <CarouselWithControls elementWidth={45} elementWidthDesktop={33.3}>
+        <CarouselWithControls elementWidth={45} elementWidthDesktop={25}>
           {items.map((val, i) => {
             const transformedDatum = transformData(val);
             const productImage = transformedDatum.image;
