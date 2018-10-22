624fb049907143e52985e6a8b773a2c4236991e6
srane15 <srane@tataunistore.com>
2018-10-22 11:35:15 +0530

    NU-538

--- src/cart/components/SavedProduct.js
+++ src/cart/components/SavedProduct.js
@@ -61,7 +61,7 @@ SavedProduct.propTypes = {
   isViewWishList: PropTypes.bool
 };
 SavedProduct.defaultProps = {
-  couponHeading: "Apply coupon",
+  couponHeading: "Check for Coupon",
   giftCardHeading: "Surprise for a special one ?",
   giftCardLabel: "Gift wrap for free",
   isViewWishList: false
