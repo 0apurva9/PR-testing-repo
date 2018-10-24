ab99a217cf97d63fd0b7b5fe818bf9bb12007d46
subash-v <subashv2096@gmail.com>
2018-10-24 11:37:24 +0530

    checkbox in square shape

--- src/general/components/CheckBox.js
+++ src/general/components/CheckBox.js
@@ -2,9 +2,17 @@ import React from "react";
 import styles from "./CheckBox.css";
 export default class CheckBox extends React.Component {
   render() {
-    let className = styles.base;
-    if (this.props.selected) {
-      className = styles.selected;
+    console.log("checkbox ", this.props.shape == "box");
+    if (this.props.shape == "box") {
+      var className = styles.boxBase;
+      if (this.props.selected) {
+        className = styles.boxSelected;
+      }
+    } else {
+      var className = styles.base;
+      if (this.props.selected) {
+        className = styles.selected;
+      }
     }
     return (
       <div
