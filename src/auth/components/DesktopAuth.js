2f1169c1655e7cc5b30dfad3f8f266decae6e243
john2193 <janardan@xelpmoc.in>
2018-10-27 13:42:45 +0530

    css and design change

--- src/auth/components/DesktopAuth.js
+++ src/auth/components/DesktopAuth.js
@@ -3,12 +3,18 @@ import styles from "./DesktopAuthStyles.css";
 import LoginContainer from "../containers/LoginContainer";
 import SignUpContainer from "../containers/SignUpContainer";
 import SocialButtonsContainer from "../containers/SocialButtonsContainer";
+import { TERMS_AND_CONDITION_URL } from "../../lib/constants";
+import { TATA_CLIQ_ROOT } from "../../lib/apiRequest.js";
 export default class DesktopAuth extends React.Component {
   handleClose() {
     if (this.props.closeModal) {
       this.props.closeModal();
     }
   }
+  redirectToHelp = url => {
+    const urlSuffix = url.replace(TATA_CLIQ_ROOT, "$1");
+    this.props.history.push(urlSuffix);
+  };
   render() {
     return (
       <div className={styles.popup}>
@@ -24,6 +30,12 @@ export default class DesktopAuth extends React.Component {
           </div>
           <div className={styles.socialButtonHolder}>
             <SocialButtonsContainer />
+            <div
+              className={styles.termsAndConditionText}
+              onClick={() => this.redirectToHelp(TERMS_AND_CONDITION_URL)}
+            >
+              By signing up, you agree to our <span>T&C</span>
+            </div>
           </div>
         </div>
       </div>
--- src/auth/components/SocialButtons.js
+++ src/auth/components/SocialButtons.js
@@ -17,137 +17,115 @@ const FACEBOOK_JSDK = "facebook-jssdk";
 const SIGN_IN_TEXT = "Sign in with your social account";
 const SIGN_UP_TEXT = "Sign Up with your social account";
 export default class SocialButtons extends Component {
-    componentDidMount() {
-        //load FaceBook Sdk
-        window.fbAsyncInit = () => {
-            window.FB.init({
-                appId: process.env.REACT_APP_FACEBOOK_CLIENT_ID,
-                cookie: true,
-                xfbml: true,
-                version: FACEBOOK_VERSION
-            });
-            window.FB.AppEvents.logPageView();
-        };
-
-        ((d, s, id) => {
-            var js,
-                fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s);
-            js.id = id;
-            js.src = FACEBOOK_SDK;
-            fjs.parentNode.insertBefore(js, fjs);
-        })(document, SCRIPT, FACEBOOK_JSDK);
-
-        //loadGoogleSdk
-        if (this.props.loadGoogleSdk) {
-            this.props.loadGoogleSdk();
-        }
-    }
-
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.user) {
-            if (nextProps.user.isLoggedIn === true) {
-                if (this.props.redirectToAfterAuthUrl) {
-                    this.props.history.replace(
-                        this.props.redirectToAfterAuthUrl
-                    );
-                    this.props.clearUrlToRedirectToAfterAuth();
-                } else {
-                    this.props.history.replace(HOME_ROUTER);
-                }
-            }
-        }
-    }
-
-    facebookLogin = () => {
-        if (this.props.signUp) {
-            this.props.facebookLogin(true);
-        } else {
-            this.props.facebookLogin(false);
-        }
-    };
-    googlePlusLogin = () => {
-        if (this.props.signUp) {
-            this.props.googlePlusLogin(this.props.signUp);
-        } else {
-            this.props.googlePlusLogin(false);
-        }
+  componentDidMount() {
+    //load FaceBook Sdk
+    window.fbAsyncInit = () => {
+      window.FB.init({
+        appId: process.env.REACT_APP_FACEBOOK_CLIENT_ID,
+        cookie: true,
+        xfbml: true,
+        version: FACEBOOK_VERSION
+      });
+      window.FB.AppEvents.logPageView();
     };
 
-    render() {
-        let width = window.innerWidth;
-        let titleText =
-            this.props.type === SOCIAL_SIGN_UP
-                ? this.props.titleTextSignIp
-                : this.props.titleTextLogin;
-        return (
-            <div className={styles.base}>
-                <div className={styles.textHolder}>
-                    <p className={styles.title}>{titleText}</p>
-                </div>
-                <MediaQuery query="(min-device-width: 1025px)">
-                    <div className={styles.base}>
-                        <div className={styles.holder}>
-                            <CircleButton
-                                color={"rgba(0,0,0,0)"}
-                                size={130}
-                                onClick={this.facebookLogin}
-                                icon={
-                                    <Icon
-                                        image={desktopFacebookImage}
-                                        size={130}
-                                    />
-                                }
-                            />
-                        </div>
-                        <div className={styles.separator} />
-                        <div className={styles.holder}>
-                            <CircleButton
-                                color={"rgba(0,0,0,0)"}
-                                size={130}
-                                onClick={this.googlePlusLogin}
-                                icon={
-                                    <Icon
-                                        image={desktopGooglePlus}
-                                        size={130}
-                                    />
-                                }
-                            />
-                        </div>
-                    </div>
-                </MediaQuery>
-                <MediaQuery query="(max-device-width: 1024px)">
-                    <div className={styles.textHolder}>
-                        <p className={styles.title}>{this.props.titleText}</p>
-                    </div>
-                    <div className={styles.base}>
-                        <div className={styles.holder}>
-                            <div
-                                className={styles.button}
-                                onClick={this.facebookLogin}
-                            >
-                                <div className={styles.iconHolderFacebook} />
-                                Facebook
-                            </div>
-                            {/* <CircleButton
+    ((d, s, id) => {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = FACEBOOK_SDK;
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, SCRIPT, FACEBOOK_JSDK);
+
+    //loadGoogleSdk
+    if (this.props.loadGoogleSdk) {
+      this.props.loadGoogleSdk();
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.user) {
+      if (nextProps.user.isLoggedIn === true) {
+        if (this.props.redirectToAfterAuthUrl) {
+          this.props.history.replace(this.props.redirectToAfterAuthUrl);
+          this.props.clearUrlToRedirectToAfterAuth();
+        } else {
+          this.props.history.replace(HOME_ROUTER);
+        }
+      }
+    }
+  }
+
+  facebookLogin = () => {
+    if (this.props.signUp) {
+      this.props.facebookLogin(true);
+    } else {
+      this.props.facebookLogin(false);
+    }
+  };
+  googlePlusLogin = () => {
+    if (this.props.signUp) {
+      this.props.googlePlusLogin(this.props.signUp);
+    } else {
+      this.props.googlePlusLogin(false);
+    }
+  };
+
+  render() {
+    let width = window.innerWidth;
+    let titleText =
+      this.props.type === SOCIAL_SIGN_UP
+        ? this.props.titleTextSignIp
+        : this.props.titleTextLogin;
+    return (
+      <div className={styles.base}>
+        <div className={styles.textHolder}>
+          <p className={styles.title}>{titleText}</p>
+        </div>
+        <MediaQuery query="(min-device-width: 1025px)">
+          <div className={styles.socialButtonHolder}>
+            <div className={styles.holder}>
+              <div
+                className={styles.facebookImageHolderDesktop}
+                onClick={this.facebookLogin}
+              />
+            </div>
+            <div className={styles.separator} />
+            <div className={styles.holder}>
+              <div
+                className={styles.googleImageForDesktop}
+                onClick={this.googlePlusLogin}
+              />
+            </div>
+          </div>
+        </MediaQuery>
+        <MediaQuery query="(max-device-width: 1024px)">
+          <div className={styles.textHolder}>
+            <p className={styles.title}>{this.props.titleText}</p>
+          </div>
+          <div className={styles.base}>
+            <div className={styles.holder}>
+              <div className={styles.button} onClick={this.facebookLogin}>
+                <div className={styles.iconHolderFacebook} />
+                Facebook
+              </div>
+              {/* <CircleButton
                 color={"rgba(0,0,0,0)"}
                 size={45}
                 onClick={this.facebookLogin}
                 icon={<Icon image={facebookImage} size={45} />}
               /> */}
-                        </div>
-                        <div className={styles.separator}>
-                            <div
-                                className={styles.button}
-                                onClick={this.googlePlusLogin}
-                            >
-                                <div className={styles.iconHolderGoogle} />
-                                Google
-                            </div>
-                        </div>
+            </div>
+            <div className={styles.separator}>
+              <div className={styles.button} onClick={this.googlePlusLogin}>
+                <div className={styles.iconHolderGoogle} />
+                Google
+              </div>
+            </div>
 
-                        {/* <div className={styles.holder}>
+            {/* <div className={styles.holder}>
               <CircleButton
                 color={"rgba(0,0,0,0)"}
                 size={45}
@@ -155,20 +133,20 @@ export default class SocialButtons extends Component {
                 icon={<Icon image={googlePlus} size={45} />}
               />
             </div> */}
-                    </div>
-                </MediaQuery>
-            </div>
-        );
-    }
+          </div>
+        </MediaQuery>
+      </div>
+    );
+  }
 }
 
 SocialButtons.propTypes = {
-    titleTextSignIp: PropTypes.string,
-    titleTextLogin: PropTypes.string,
-    facebookLogin: PropTypes.func,
-    googlePlusLogin: PropTypes.func
+  titleTextSignIp: PropTypes.string,
+  titleTextLogin: PropTypes.string,
+  facebookLogin: PropTypes.func,
+  googlePlusLogin: PropTypes.func
 };
 SocialButtons.defaultProps = {
-    titleTextLogin: SIGN_IN_TEXT,
-    titleTextSignIp: SIGN_UP_TEXT
+  titleTextLogin: SIGN_IN_TEXT,
+  titleTextSignIp: SIGN_UP_TEXT
 };
