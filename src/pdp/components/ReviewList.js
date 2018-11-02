fd01d4a8864791620c36903ec60855081cf030ed
srane15 <srane@tataunistore.com>
2018-11-02 16:26:27 +0530

    email masking done

--- src/pdp/components/ReviewList.js
+++ src/pdp/components/ReviewList.js
@@ -2,6 +2,19 @@ import React from "react";
 import styles from "./ReviewList.css";
 import ReviewPage from "./ReviewPage";
 import PropTypes from "prop-types";
+function validateEmail(email) {
+  let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+function protect_email(user_email) {
+  var avg, splitted, part1, part2;
+  splitted = user_email.split("@");
+  part1 = splitted[0];
+  avg = part1.length / 2;
+  part1 = part1.substring(0, part1.length - avg);
+  part2 = splitted[1];
+  return part1 + "***@" + part2;
+}
 export default class ReviewList extends React.Component {
   render() {
     return (
@@ -16,6 +29,9 @@ export default class ReviewList extends React.Component {
               data.principal &&
               data.principal.name &&
               data.principal.name.trim();
+            if (validateEmail(userName)) {
+              userName = protect_email(userName);
+            }
             return (
               <ReviewPage
                 rating={data && data.rating}
