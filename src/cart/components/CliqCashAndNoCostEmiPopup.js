d101ab8a6ced1b0c8bc177e1cc4971457102994d
InshaNazeer <insha@xelpmoc.in>
2018-11-15 10:51:17 +0530

    make no cost emi pop up for cliq cash

--- src/cart/components/CliqCashAndNoCostEmiPopup.js
+++ src/cart/components/CliqCashAndNoCostEmiPopup.js
@@ -3,7 +3,9 @@ import PropTypes from "prop-types";
 import Button from "../../general/components/Button.js";
 import styles from "./CliqCashAndNoCostEmiPopup.css";
 import { NO_COST_EMI_COUPON, SUCCESS } from "../../lib/constants";
-
+import BottomSlideModal from "../../general/components/BottomSlideModal";
+import MobileOnly from "../../general/components/MobileOnly";
+import DesktopOnly from "../../general/components/DesktopOnly";
 export default class CliqCashAndNoCostEmiPopup extends React.Component {
   continueWithNoCostEmi() {
     if (this.props.continueWithNoCostEmi) {
@@ -22,41 +24,84 @@ export default class CliqCashAndNoCostEmiPopup extends React.Component {
   }
   render() {
     return (
-      <div className={styles.base}>
-        <div className={styles.paymentMethodDescription}>
-          <div className={styles.heading}>
-            Cliq Cash and No Cost Emi can not use together
+      <React.Fragment>
+        <MobileOnly>
+          <div className={styles.base}>
+            <div className={styles.paymentMethodDescription}>
+              <div className={styles.heading}>
+                Cliq Cash and No Cost Emi can not use together
+              </div>
+              <div className={styles.subHeading}>
+                Please choose to use your CliQ Cash balance or continue with No
+                Cost EMI
+              </div>
+            </div>
+            <div className={styles.buttonHolderForPaymentMode}>
+              <div className={styles.button}>
+                <Button
+                  type="primary"
+                  backgroundColor="#ff1744"
+                  height={36}
+                  label="Continue with No Cost Emi"
+                  width={211}
+                  textStyle={{ color: "#FFF", fontSize: 14 }}
+                  onClick={() => this.continueWithNoCostEmi()}
+                />
+              </div>
+            </div>
+            <div className={styles.buttonHolderForContinueCoupon}>
+              <div className={styles.button}>
+                <Button
+                  type="secondary"
+                  height={36}
+                  label="Use Cliq Cash"
+                  width={211}
+                  onClick={() => this.useCliqCash()}
+                />
+              </div>
+            </div>
           </div>
-          <div className={styles.subHeading}>
-            Please choose to use your CliQ Cash balance or continue with No Cost
-            EMI
-          </div>
-        </div>
-        <div className={styles.buttonHolderForPaymentMode}>
-          <div className={styles.button}>
-            <Button
-              type="primary"
-              backgroundColor="#ff1744"
-              height={36}
-              label="Continue with No Cost Emi"
-              width={211}
-              textStyle={{ color: "#FFF", fontSize: 14 }}
-              onClick={() => this.continueWithNoCostEmi()}
-            />
-          </div>
-        </div>
-        <div className={styles.buttonHolderForContinueCoupon}>
-          <div className={styles.button}>
-            <Button
-              type="secondary"
-              height={36}
-              label="Use Cliq Cash"
-              width={211}
-              onClick={() => this.useCliqCash()}
-            />
-          </div>
-        </div>
-      </div>
+        </MobileOnly>
+        <DesktopOnly>
+          <BottomSlideModal>
+            <div className={styles.base}>
+              <div className={styles.paymentMethodDescription}>
+                <div className={styles.heading}>
+                  Cliq Cash and No Cost Emi can not use together
+                </div>
+                <div className={styles.subHeading}>
+                  Please choose to use your CliQ Cash balance or continue with
+                  No Cost EMI
+                </div>
+              </div>
+              <div className={styles.buttonHolderForPaymentMode}>
+                <div className={styles.button}>
+                  <Button
+                    type="primary"
+                    backgroundColor="#ff1744"
+                    height={36}
+                    label="Continue with No Cost Emi"
+                    width={211}
+                    textStyle={{ color: "#FFF", fontSize: 14 }}
+                    onClick={() => this.continueWithNoCostEmi()}
+                  />
+                </div>
+              </div>
+              <div className={styles.buttonHolderForContinueCoupon}>
+                <div className={styles.button}>
+                  <Button
+                    type="secondary"
+                    height={36}
+                    label="Use Cliq Cash"
+                    width={211}
+                    onClick={() => this.useCliqCash()}
+                  />
+                </div>
+              </div>
+            </div>
+          </BottomSlideModal>
+        </DesktopOnly>
+      </React.Fragment>
     );
   }
 }
