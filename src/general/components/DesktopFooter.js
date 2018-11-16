b42213826b57e6d58936c5005205167d5cb5fe60
john-ghosh-xelp <janardan@xelpmoc.in>
2018-11-16 18:58:37 +0530

    text change

--- src/general/components/DesktopFooter.js
+++ src/general/components/DesktopFooter.js
@@ -13,7 +13,7 @@ import {
   ADOBE_DIRECT_CALL_FOR_SOCIALMEDIA_CLICK,
   ADOBE_DIRECT_CALL_FOR_FOOTER_SUBSCRIBE
 } from "../../lib/adobeUtils";
-const TEXT = "© 2017 Tata CLiQ | All rights reserved";
+const TEXT = "© 2018 Tata CLiQ | All rights reserved";
 class DesktopFooter extends React.Component {
   componentDidMount() {
     let currentUrl = this.props.location.pathname;
