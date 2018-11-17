05d22483ad003131a39a6cb2bbc37a9ac564bca3
john-ghosh-xelp <janardan@xelpmoc.in>
2018-11-17 16:23:14 +0530

    remove console

--- src/home/components/DiscoverMoreComponentDesktop.js
+++ src/home/components/DiscoverMoreComponentDesktop.js
@@ -31,7 +31,6 @@ export default class DiscoverMoreComponentDesktop extends React.Component {
     });
   };
   render() {
-    console.log(this.props);
     return (
       <div className={styles.base}>
         <div className={styles.imageHolder}>
