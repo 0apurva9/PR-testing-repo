e66a1ea42a8c342aff6e76b1d032828a2b4eaf8f
InshaNazeer <insha@xelpmoc.in>
2018-11-21 10:41:05 +0530

    solve bank offer issue when user apply cliq cash

--- src/cart/components/CheckOutPage.js
+++ src/cart/components/CheckOutPage.js
@@ -2007,7 +2007,6 @@ if you have order id in local storage then you have to show order confirmation p
       }
     }
   };
-
   removeCliqCash = () => {
     this.setState({
       isCliqCashApplied: false,
