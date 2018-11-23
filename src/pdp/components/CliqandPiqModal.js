1f40a53eb062e4a0080fdf425ae3d86e05e1a861
Banupriya <banupriya@xelpmoc.in>
2018-11-23 14:28:14 +0530

    handled placeholder

--- src/pdp/components/CliqandPiqModal.js
+++ src/pdp/components/CliqandPiqModal.js
@@ -206,12 +206,11 @@ export default class ReturnToStore extends React.Component {
                       : ""
                   }`}
                   disabled={this.props.pinCodeUpdateDisabled}
-                  pincode={
-                    this.props.pincode
-                      ? this.props.pinCode
-                      : this.props.productDetails &&
-                        this.props.productDetails.isServiceableToPincode &&
-                        this.props.productDetails.isServiceableToPincode.pinCode
+                  pincode={this.props.pincode}
+                  ServiceablePincode={
+                    this.props.productDetails &&
+                    this.props.productDetails.isServiceableToPincode &&
+                    this.props.productDetails.isServiceableToPincode.pinCode
                   }
                   changePincode={pincode => this.getPinCodeDetails(pincode)}
                 />
