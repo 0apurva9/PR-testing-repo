9a7a3fcee46f6db63dcedf8c8719f1144ebfac33
john-ghosh-xelp <janardan@xelpmoc.in>
2018-11-30 13:57:57 +0530

    remove console log

--- src/general/components/NewBrandDesktop.js
+++ src/general/components/NewBrandDesktop.js
@@ -9,7 +9,6 @@ export default class NewBrandDesktop extends React.Component {
     }
   }
   render() {
-    console.log(this.props.data);
     return (
       <div className={styles.base}>
         {this.props &&
