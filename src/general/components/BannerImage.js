cbd04b3544a135b2d7aaa7e435524dbdcd0f9c26
Subash V <subashv2096@gmail.com>
2018-12-03 13:02:00 +0530

    added anchor tag on banner

--- src/general/components/BannerImage.js
+++ src/general/components/BannerImage.js
@@ -7,7 +7,8 @@ import MediaQuery from "react-responsive";
 import PropTypes from "prop-types";
 import { TATA_CLIQ_ROOT } from "../../lib/apiRequest.js";
 export default class Banner extends React.Component {
-  onClick = () => {
+  onClick(event) {
+    event.preventDefault();
     if (this.props.url) {
       const urlSuffix = this.props.url.replace(TATA_CLIQ_ROOT, "$1");
       const urlPath = new URL(this.props.url).pathname;
@@ -21,42 +22,45 @@ export default class Banner extends React.Component {
     if (this.props.setClickedElementId) {
       this.props.setClickedElementId();
     }
-  };
+  }
   render() {
+    const urlSuffix = this.props.url.replace(TATA_CLIQ_ROOT, "$1");
     return (
       <React.Fragment>
         <MediaQuery query="(min-device-width: 1025px)">
-          <div className={styles.base} onClick={this.onClick}>
-            <div className={styles.flexImageHolder}>
-              <ImageFlexible image={this.props.image} />
-            </div>
-            <div className={styles.content}>
-              <div className={styles.logoAndText}>
-                <div
-                  className={styles.logo}
-                  style={{ backgroundImage: `url(${this.props.logo})` }}
-                />
-                {this.props.title && (
-                  <div className={styles.title}>{this.props.title}</div>
-                )}
-                {this.props.subTitle && (
-                  <div className={styles.subTitle}>{this.props.subTitle}</div>
-                )}
-
-                {this.props.showButton &&
-                  this.props.buttonLabel && (
-                    <div className={styles.button}>
-                      <Button
-                        width={195}
-                        height={46}
-                        label={this.props.buttonLabel}
-                        type={"primary"}
-                      />
-                    </div>
+          <a href={urlSuffix} target="_blank">
+            <div className={styles.base} onClick={event => this.onClick(event)}>
+              <div className={styles.flexImageHolder}>
+                <ImageFlexible image={this.props.image} />
+              </div>
+              <div className={styles.content}>
+                <div className={styles.logoAndText}>
+                  <div
+                    className={styles.logo}
+                    style={{ backgroundImage: `url(${this.props.logo})` }}
+                  />
+                  {this.props.title && (
+                    <div className={styles.title}>{this.props.title}</div>
                   )}
+                  {this.props.subTitle && (
+                    <div className={styles.subTitle}>{this.props.subTitle}</div>
+                  )}
+
+                  {this.props.showButton &&
+                    this.props.buttonLabel && (
+                      <div className={styles.button}>
+                        <Button
+                          width={195}
+                          height={46}
+                          label={this.props.buttonLabel}
+                          type={"primary"}
+                        />
+                      </div>
+                    )}
+                </div>
               </div>
             </div>
-          </div>
+          </a>
           {(this.props.isFollow || this.props.newProducts) && (
             <div className={styles.followButtonWithText}>
               {this.props.isFollow && (
