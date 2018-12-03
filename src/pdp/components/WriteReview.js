a7cd166623b06169436affee91ef4fd10730a0b6
Banupriya <banupriya@xelpmoc.in>
2018-12-03 18:51:06 +0530

    after submiting review url redirect to pdp page

--- src/pdp/components/WriteReview.js
+++ src/pdp/components/WriteReview.js
@@ -84,10 +84,10 @@ class WriteReview extends React.Component {
           if (this.state.resetRating === true) {
             this.setState({ resetRating: false });
           }
-          let url = this.props.location.pathname;
-          url = url.replace("/write-review", "");
-          this.props.history.push(url);
         }
+        let url = this.props.location.pathname;
+        url = url.replace("/write-review", "");
+        this.props.history.push(url);
       }
     } else {
       const url = this.props.location.pathname;
