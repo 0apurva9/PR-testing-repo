5ec72cb876f827e5be2bb78a7b1d22191a76dd43
Banupriya <banupriya@xelpmoc.in>
2018-12-10 18:23:06 +0530

    added changes

--- src/search/components/SearchPage.js
+++ src/search/components/SearchPage.js
@@ -196,7 +196,7 @@ export default class SearchPage extends React.Component {
       : true;
   }
   handleOnSearchString(searchString) {
-    var format = /[!@#$%^&*()_+\=\[\]{};:\\|,.<>\/?]+/;
+    var format = /[!@#$%^&*_+\=\[\]{};:\\|.<>\/?]+/;
     if (
       /\s*[0-9a-zA-z]+/.test(searchString) &&
       !format.test(searchString) &&
