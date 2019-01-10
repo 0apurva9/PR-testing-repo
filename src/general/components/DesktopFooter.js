5c265d8b49355d24a4fe6d2501728384c2072130
srane15 <srane@tataunistore.com>
2019-01-10 19:28:16 +0530

    footer issue fixed

--- src/general/components/DesktopFooter.js
+++ src/general/components/DesktopFooter.js
@@ -168,6 +168,7 @@ class DesktopFooter extends React.Component {
                 footerData.items[0] &&
                 footerData.items[0].socialLinks &&
                 footerData.items[0].socialLinks[1] &&
+                footerData.items[0].socialLinks[1].list &&
                 footerData.items[0].socialLinks[1].list.map((val, i) => {
                   return (
                     <a href={val.webUrl} target="_blank">
@@ -184,6 +185,7 @@ class DesktopFooter extends React.Component {
                 footerData.items[0] &&
                 footerData.items[0].socialLinks &&
                 footerData.items[0].socialLinks[0] &&
+                footerData.items[0].socialLinks[0].list &&
                 footerData.items[0].socialLinks[0].list.map((val, i) => {
                   return (
                     <a href={val.webUrl} target="_blank">
