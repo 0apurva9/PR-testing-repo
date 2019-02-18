5a5b492f057819c6a31f76133bf64a7c949df896
Shriney Malekar <smalekar@tataunistore.com>
2019-02-18 18:52:57 +0530

    Removed unused method getCODEligibility

--- src/cart/components/CheckOutPage.js
+++ src/cart/components/CheckOutPage.js
@@ -3341,7 +3341,6 @@ if you have order id in local storage then you have to show order confirmation p
                       }
                       selectPayPal={val => this.selectPayPal(val)}
                       displayToast={message => this.props.displayToast(message)}
-                      getCODEligibility={() => this.getCODEligibility()}
                       getNetBankDetails={() => this.getNetBankDetails()}
                       getEmiBankDetails={() => this.getEmiBankDetails()}
                       getEmiEligibility={() => this.getEmiEligibility()}
