d0b92d943a3ac5b9ba9693f7124dabdd0d52949d
srane15 <srane@tataunistore.com>
2019-02-23 19:06:47 +0530

    maintenance page updated with new img

--- src/errorsPage/components/Maintenance.js
+++ src/errorsPage/components/Maintenance.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./Maintenance.css";
-
 export default class Maintenance extends React.Component {
   render() {
     return (
@@ -10,25 +9,7 @@ export default class Maintenance extends React.Component {
             <div className={styles.logoHolder} />
           </div>
         </div>
-        <div className={styles.base}>
-          <div className={styles.headerWithTextHolder}>
-            {/* <div className={styles.pageTitle}>502</div> */}
-            <div className={styles.infoText}>
-              Our site is going through an upgrade. Do visit us in a few hours.{" "}
-            </div>
-            <div className={styles.infoText}>
-              In the meantime please call us at{" "}
-              <a href="tel:+919029108282" className={styles.linkColor}>
-                9029108282
-              </a>{" "}
-              or email us at{" "}
-              <a href="mailto:hello@tatacliq.com" className={styles.linkColor}>
-                hello@tatacliq.com
-              </a>{" "}
-              if you have any queries.
-            </div>
-          </div>
-        </div>
+        <div className={styles.base} />
       </React.Fragment>
     );
   }
