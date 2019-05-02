9d20446ca5779ea6064311e42ca3253f5a289f84
insha-nazeer-xelp <insha@xelpmoc.in>
2019-05-02 10:43:46 +0530

    change heading in modal

--- src/pdp/components/CliqandPiqModal.js
+++ src/pdp/components/CliqandPiqModal.js
@@ -190,7 +190,7 @@ export default class ReturnToStore extends React.Component {
       return (
         <div className={styles.base}>
           <div className={styles.mapButton}>
-            <div className={styles.cliqPiqText}>QUiQ PiQ</div>
+            <div className={styles.cliqPiqText}>CliQ n PiQ</div>
             <div
               className={styles.closeButton}
               onClick={() => this.CloseCliqAndPiqModal()}
