02822db7e7863dafdce3f6214587977f5970ffff
Shriney Malekar <smalekar@tataunistore.com>
2019-05-08 19:22:07 +0530

    Minor code changes for creating cookie

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -379,6 +379,10 @@ async function replaceOldCartCookieForLoggedInUser(url, newCustomerCookie) {
     Cookie.getCookie(CART_DETAILS_FOR_LOGGED_IN_USER)
   );
   Cookie.deleteCookie(CART_DETAILS_FOR_LOGGED_IN_USER);
+  Cookie.createCookie(
+    CART_DETAILS_FOR_LOGGED_IN_USER,
+    JSON.stringify(newCustomerCookie)
+  );
   return url.replace(oldCustomerCookie.code, newCustomerCookie.code);
 }
 
