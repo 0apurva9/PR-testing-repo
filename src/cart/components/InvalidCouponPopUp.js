642d6e50dd57065e71c4ec66fcebff6c87b40c46
Shriney Malekar <smalekar@tataunistore.com>
2019-05-24 00:18:55 +0530

    Coupon code issue resolved

--- src/cart/components/InvalidCouponPopUp.js
+++ src/cart/components/InvalidCouponPopUp.js
@@ -158,10 +158,39 @@ export default class InvalidCouponPopUp extends React.Component {
         this.props.closeModal();
       }
     }
-    if (releaseStatus.status === SUCCESS) {
-      this.props.closeModal();
-    }
     if (!bankCouponCode || !userCouponCode || !noCostEmiCoupon) {
+      if (
+        !releaseStatus &&
+        this.props.result &&
+        this.props.result.bankOffer &&
+        this.props.result.bankOffer.couponCode
+      ) {
+        releaseStatus = await this.props.releaseBankOffer(
+          this.props.result.bankOffer.couponCode
+        );
+      }
+      if (
+        !releaseStatus &&
+        this.props.result &&
+        this.props.result.userCoupon &&
+        this.props.result.userCoupon.couponCode
+      ) {
+        releaseStatus = await this.props.releaseUserCoupon(
+          this.props.result.userCoupon.couponCode
+        );
+      }
+      if (
+        !releaseStatus &&
+        this.props.result &&
+        this.props.result.noCostEmiCoupon &&
+        this.props.result.noCostEmiCoupon.couponCode
+      ) {
+        releaseStatus = await this.props.releaseNoCostEmiCoupon(
+          this.props.result.userCoupon.couponCode
+        );
+      }
+    }
+    if (releaseStatus.status === SUCCESS) {
       this.props.closeModal();
     }
   }
