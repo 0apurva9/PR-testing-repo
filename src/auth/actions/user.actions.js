43b1d01ecc7da124c145d4161a61062026f443f5
Chandan Singh <csingh@tataunistore.com>
2019-05-27 19:12:40 +0530

    Revert back all wrong changes Google auth

--- src/auth/actions/user.actions.js
+++ src/auth/actions/user.actions.js
@@ -734,19 +734,8 @@ export function generateCustomerLevelAccessTokenForSocialMedia(
       );
       const resultJson = await result.json();
       const resultJsonStatus = ErrorHandling.getFailureResponse(resultJson);
-      console.log("=====================resultJson========>", resultJson);
-      console.log(
-        "=====================resultJsonStatus========>",
-        resultJsonStatus
-      );
-      if (
-        resultJsonStatus.status &&
-        resultJsonStatus.status.errors &&
-        resultJsonStatus.status.errors[0].type === "InvalidGrantError"
-      ) {
-        console.log("===========This should not come===================>");
-      } else if (resultJsonStatus.status) {
-        console.log("===========Yoooo===================>");
+
+      if (resultJsonStatus.status) {
         throw new Error(resultJsonStatus.message);
       }
 
