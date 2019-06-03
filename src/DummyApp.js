4b4f42e541b80e1769bc2cfc2b66d674acb53c0f
Subash V <subashv2096@gmail.com>
2019-06-03 12:06:02 +0530

    added loader

--- src/DummyApp.js
+++ src/DummyApp.js
@@ -1,6 +1,7 @@
 import React from "react";
-import HeaderContainer from "./general/containers/HeaderContainer";
-import DesktopFooterContainer from "./general/containers/DesktopFooterContainer";
+// import HeaderContainer from "./general/containers/HeaderContainer";
+// import DesktopFooterContainer from "./general/containers/DesktopFooterContainer";
+import Loader from "./general/components/Loader";
 // import PropTypes from "prop-types";
 export default class DummyApp extends React.Component {
   render() {
@@ -9,11 +10,10 @@ export default class DummyApp extends React.Component {
         className="ok"
         style={{
           width: "100%",
-          height: "100vh",
-          backgroundColor: "#ffffff"
+          height: "100vh"
         }}
       >
-        <DesktopFooterContainer />
+        <Loader />
       </div>
     );
   }
