f76cde9d5d12130808974f4c016b63e82ac050ee
banupriya <banupriya@xelpmoc.in>
2019-07-11 14:42:09 +0530

    handled all the condition

--- src/account/components/MyAccountWrapper.js
+++ src/account/components/MyAccountWrapper.js
@@ -50,15 +50,14 @@ export default class MyAccountWrapper extends React.Component {
   }
   navigateToLogin() {
     const url = this.props.location.pathname;
-    if (
-      url ===
-      `${MY_ACCOUNT_PAGE}${MY_ACCOUNT_CLIQ_CASH_PAGE}${TRANSACTION_HISTORY}`
-    ) {
+    if (url.match(/cliq-cash/g)) {
       this.props.setUrlToRedirectToAfterAuth(
         `${MY_ACCOUNT_PAGE}${MY_ACCOUNT_CLIQ_CASH_PAGE}`
       );
-    } else {
+    } else if (url === `${MY_ACCOUNT_PAGE}${MY_ACCOUNT_GIFT_CARD_PAGE}`) {
       this.props.setUrlToRedirectToAfterAuth(`${url}`);
+    } else {
+      this.props.setUrlToRedirectToAfterAuth(`${MY_ACCOUNT_PAGE}`);
     }
     this.props.history.push(LOGIN_PATH);
     return null;
