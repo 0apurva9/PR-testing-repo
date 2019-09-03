a50b7cdaacd2777b65514f766e5aba1ab49ab7f6
akash-singh-xelp <akash.kumar@xelpmoc.in>
2019-09-03 17:08:02 +0530

    changes for ssr deployment

---
+++ ecosystem.ssr.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      name: "tata-cliq-frontend",
+      script: "build/server/static/js/main.js",
+      watch: true,
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ],
+  deploy: {
+    ssr: {
+      user: "ubuntu",
+      host: "54.147.12.99",
+      key: "~/.ssh/ORACLE-HYBRIS.pem",
+      ref: "origin/desktop",
+      repo: "git@github.com:XelpmocDesignandTechPvtLtd/tata-cliq-frontend.git",
+      ssh_options: ["StrictHostKeyChecking=no", "PasswordAuthentication=no"],
+      path: "/home/ubuntu/tata-cliq-frontend",
+      "post-deploy":
+        "yarn install && yarn run build && yarn run build:server && yarn run pre-build-ssr"
+    }
+  }
+};
