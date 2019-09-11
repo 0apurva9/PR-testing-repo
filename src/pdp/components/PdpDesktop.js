fa664c24bcc61d406c8f2db3431cdc2789b0972f
Chandan Singh <csingh@tataunistore.com>
2019-09-11 09:59:29 +0530

    Samsung chat icon resolved

--- src/pdp/components/PdpDesktop.js
+++ src/pdp/components/PdpDesktop.js
@@ -668,7 +668,8 @@ export default class PdpApparel extends React.Component {
     const productData = this.props.productDetails;
 
     const manufacturerDetails = this.props.manufacturerDetails;
-
+    let mshProduct = productData && productData.brandURL;
+    console.log("productData", mshProduct);
     const tailedKnowMoreV2 =
       productData &&
       productData.knowMoreV2 &&
@@ -1211,31 +1212,33 @@ export default class PdpApparel extends React.Component {
                   )}
                 </div>
                 <div>
-                  <div className={styles.sumsungSeparator}>
-                    <div className={styles.chatIcon}>
-                      {productData.brandName === "Samsung" ? (
-                        <a
-                          href={samsungChatUrl}
-                          target="_blank"
-                          className={styles.samsungChatImgHolder}
-                        >
-                          <img
-                            src="https://assets.tatacliq.com/medias/sys_master/images/11437918060574.png"
-                            alt="Samsung Chat"
-                          />
-                        </a>
-                      ) : null}
-                      <div className={styles.chatText}>
-                        <p>
-                          Chat with the Samsung brand representative directly
-                          for more info
-                        </p>
-                        <a href={samsungChatUrl} target="_blank">
-                          Click here to chat
-                        </a>
+                  {mshProduct.includes("samsung") && (
+                    <div className={styles.sumsungSeparator}>
+                      <div className={styles.chatIcon}>
+                        {productData.brandName === "Samsung" ? (
+                          <a
+                            href={samsungChatUrl}
+                            target="_blank"
+                            className={styles.samsungChatImgHolder}
+                          >
+                            <img
+                              src="https://assets.tatacliq.com/medias/sys_master/images/11437918060574.png"
+                              alt="Samsung Chat"
+                            />
+                          </a>
+                        ) : null}
+                        <div className={styles.chatText}>
+                          <p>
+                            Chat with the Samsung brand representative directly
+                            for more info
+                          </p>
+                          <a href={samsungChatUrl} target="_blank">
+                            Click here to chat
+                          </a>
+                        </div>
                       </div>
                     </div>
-                  </div>
+                  )}
                 </div>
               </div>
             </div>
