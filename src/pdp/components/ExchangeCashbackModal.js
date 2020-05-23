e5131ba0f864290932973c5eb4522c86bdca78e2
Siddhesh Rane <srane@tataunistore.com>
2020-05-23 19:05:42 +0530

    Modal - sticky header added

--- src/pdp/components/ExchangeCashbackModal.js
+++ src/pdp/components/ExchangeCashbackModal.js
@@ -11,38 +11,42 @@ export default class ExchangeCashbackModal extends React.Component {
   render() {
     return (
       <div className={styles.howExchangeWorksContainer}>
-        <div
-          className={styles.modalBackArrow}
-          onClick={() => this.closeCashbackModal()}
-        />
-        <div className={styles.howExchangeWorksHeading}>Cashback Details</div>
-        <ul className={styles.listDetails}>
-          <li>
-            After placing the order for your new phone along with exchange
-            offer. Remember to select your preferred payment mode for receiving
-            Exchange Cashback
-          </li>
-          <li>
-            Exchange Cashback would be credited to your preferred mode within 2
-            business days after delivery of your new product and pickup of the
-            old product
-          </li>
-          <li>
-            In case of the new phone being returned, the amount refunded would
-            be after deducting the{" "}
-            <span className={styles.fontRegular}>"CLiQ Exclusive"</span>{" "}
-            Cashback.
-          </li>
-          <li>
-            To know more, refer{" "}
-            <span
-              className={styles.tncText}
-              onClick={() => this.openTnCModal()}
-            >
-              T&amp;C
-            </span>
-          </li>
-        </ul>
+        <div className={styles.topContainer}>
+          <div
+            className={styles.modalBackArrow}
+            onClick={() => this.closeCashbackModal()}
+          />
+          <div className={styles.howExchangeWorksHeading}>Cashback Details</div>
+        </div>
+        <div className={styles.bottomContainer}>
+          <ul className={styles.listDetails}>
+            <li>
+              After placing the order for your new phone along with exchange
+              offer. Remember to select your preferred payment mode for
+              receiving Exchange Cashback
+            </li>
+            <li>
+              Exchange Cashback would be credited to your preferred mode within
+              2 business days after delivery of your new product and pickup of
+              the old product
+            </li>
+            <li>
+              In case of the new phone being returned, the amount refunded would
+              be after deducting the{" "}
+              <span className={styles.fontRegular}>"CLiQ Exclusive"</span>{" "}
+              Cashback.
+            </li>
+            <li>
+              To know more, refer{" "}
+              <span
+                className={styles.tncText}
+                onClick={() => this.openTnCModal()}
+              >
+                T&amp;C
+              </span>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
--- src/pdp/components/HowExchangeModalWorks.js
+++ src/pdp/components/HowExchangeModalWorks.js
@@ -13,14 +13,16 @@ export default class HowExchangeModalWorks extends React.Component {
   render() {
     return (
       <div className={styles.howExchangeWorksContainer}>
-        <div
-          className={styles.modalBackArrow}
-          onClick={() => this.closeHowExchangeWorksModal()}
-        />
-        <div className={styles.howExchangeWorksHeading}>
-          How exchange works?
+        <div className={styles.topContainer}>
+          <div
+            className={styles.modalBackArrow}
+            onClick={() => this.closeHowExchangeWorksModal()}
+          />
+          <div className={styles.howExchangeWorksHeading}>
+            How exchange works?
+          </div>
         </div>
-        <div>
+        <div className={styles.bottomContainer}>
           <img src={hew1} alt="" className={styles.iconSize} />
           <div className={styles.contentContainer}>
             <div className={styles.contentHeading}>
@@ -87,149 +89,153 @@ export default class HowExchangeModalWorks extends React.Component {
               business days
             </div>
           </div>
-        </div>
-        <div className={styles.contentHeading}>
-          The exchange product will be rejected if,
-        </div>
-        <div className={styles.contentContainerHEW}>
-          <div className={styles.contentDescriptionHEW}>
-            a) The phone does not power on
-          </div>
-          <div className={styles.contentDescriptionHEW}>
-            b) The brand or model is different from what was mentioned by you on
-            Tata CLiQ at the time of purchase of your new phone
-          </div>
-          <div className={styles.contentDescriptionHEW}>
-            c) Screen lock (all forms of screen lock viz. pattern, pin,
-            password, fingerprint, face recognition, smart lock, etc. need to be
-            deactivated) or iCloud lock (Apple) is not removed
-          </div>
-          <div className={styles.contentDescriptionHEW}>
-            d) IMEI No., on dialing *#06#, does not match with that provided by
-            you on Tata CLiQ at the time of purchase of your new phone
-          </div>
-        </div>
-        <div className={styles.contentHeadingHEW}>Questions? / FAQs</div>
-        <div className={styles.contentContainerHEW}>
           <div className={styles.contentHeading}>
-            1) How do I know if my device is eligible for the Exchange Offer?
+            The exchange product will be rejected if,
           </div>
-          <div className={styles.contentDescriptionHEW}>
-            To verify if your old phone qualifies for the Exchange Offer, check
-            the list of eligible devices against the new phone you are buying.
+          <div className={styles.contentContainerHEW}>
+            <div className={styles.contentDescriptionHEW}>
+              a) The phone does not power on
+            </div>
+            <div className={styles.contentDescriptionHEW}>
+              b) The brand or model is different from what was mentioned by you
+              on Tata CLiQ at the time of purchase of your new phone
+            </div>
+            <div className={styles.contentDescriptionHEW}>
+              c) Screen lock (all forms of screen lock viz. pattern, pin,
+              password, fingerprint, face recognition, smart lock, etc. need to
+              be deactivated) or iCloud lock (Apple) is not removed
+            </div>
+            <div className={styles.contentDescriptionHEW}>
+              d) IMEI No., on dialing *#06#, does not match with that provided
+              by you on Tata CLiQ at the time of purchase of your new phone
+            </div>
           </div>
-        </div>
-        <div className={styles.contentContainerHEW}>
-          <div className={styles.contentHeading}>
-            2) How do I avail the Exchange Offer?
+          <div className={styles.contentHeadingHEW}>Questions? / FAQs</div>
+          <div className={styles.contentContainerHEW}>
+            <div className={styles.contentHeading}>
+              1) How do I know if my device is eligible for the Exchange Offer?
+            </div>
+            <div className={styles.contentDescriptionHEW}>
+              To verify if your old phone qualifies for the Exchange Offer,
+              check the list of eligible devices against the new phone you are
+              buying.
+            </div>
           </div>
-          <div className={styles.contentDescriptionHEW}>
-            Submit your old phone details, check the proposed Exchange Cashback
-            and place order for your new phone. Pick-up of your old phone will
-            be scheduled after delivery of your new phone, and you will receive
-            your Exchange Cashback within 2 working days of pick-up
-            confirmation.
+          <div className={styles.contentContainerHEW}>
+            <div className={styles.contentHeading}>
+              2) How do I avail the Exchange Offer?
+            </div>
+            <div className={styles.contentDescriptionHEW}>
+              Submit your old phone details, check the proposed Exchange
+              Cashback and place order for your new phone. Pick-up of your old
+              phone will be scheduled after delivery of your new phone, and you
+              will receive your Exchange Cashback within 2 working days of
+              pick-up confirmation.
+            </div>
           </div>
-        </div>
-        <div className={styles.contentContainerHEW}>
-          <div className={styles.contentHeading}>
-            3) How do I send my old phone back?
+          <div className={styles.contentContainerHEW}>
+            <div className={styles.contentHeading}>
+              3) How do I send my old phone back?
+            </div>
+            <div className={styles.contentDescriptionHEW}>
+              Your old phone will be picked up by us after your new one is
+              delivered. Kindly keep your old phone ready with all your data
+              backed up, and screen lock, iCloud account lock removed on your
+              device.
+            </div>
           </div>
-          <div className={styles.contentDescriptionHEW}>
-            Your old phone will be picked up by us after your new one is
-            delivered. Kindly keep your old phone ready with all your data
-            backed up, and screen lock, iCloud account lock removed on your
-            device.
+          <div className={styles.contentContainerHEW}>
+            <div className={styles.contentHeading}>
+              4) How do I find the IMEI number of my mobile phone?
+            </div>
+            <div className={styles.contentDescriptionHEW}>
+              The IMEI number can be found by dialing <i>*#06#</i> on your phone
+            </div>
           </div>
-        </div>
-        <div className={styles.contentContainerHEW}>
-          <div className={styles.contentHeading}>
-            4) How do I find the IMEI number of my mobile phone?
+          <div className={styles.contentContainerHEW}>
+            <div className={styles.contentHeading}>
+              5) The phone I'm exchanging is a dual SIM device. Do I have to
+              provide both the IMEI numbers while filling the Exchange Offer
+              form?
+            </div>
+            <div className={styles.contentDescriptionHEW}>
+              You need to mention only one of the IMEI numbers while filling the
+              form.
+            </div>
           </div>
-          <div className={styles.contentDescriptionHEW}>
-            The IMEI number can be found by dialing <i>*#06#</i> on your phone
+          <div className={styles.contentContainerHEW}>
+            <div className={styles.contentHeading}>
+              6) Should the device be in working condition to qualify for this
+              offer?
+            </div>
+            <div className={styles.contentDescriptionHEW}>
+              Yes. The phone needs to be in working condition. It also needs to
+              have all screen locks disabled and the iCloud Account unlocked to
+              enable pick-up by our partners.
+            </div>
           </div>
-        </div>
-        <div className={styles.contentContainerHEW}>
-          <div className={styles.contentHeading}>
-            5) The phone I'm exchanging is a dual SIM device. Do I have to
-            provide both the IMEI numbers while filling the Exchange Offer form?
+          <div className={styles.contentContainerHEW}>
+            <div className={styles.contentHeading}>
+              7) Can I exchange more than one device towards a purchase?
+            </div>
+            <div className={styles.contentDescriptionHEW}>
+              No. You may only exchange one device at a time.
+            </div>
           </div>
-          <div className={styles.contentDescriptionHEW}>
-            You need to mention only one of the IMEI numbers while filling the
-            form.
+          <div className={styles.contentContainerHEW}>
+            <div className={styles.contentHeading}>
+              8) Can I provide one address for pick up and another for delivery?
+            </div>
+            <div className={styles.contentDescriptionHEW}>
+              No. The delivery address and pick-up address must be the same.
+            </div>
           </div>
-        </div>
-        <div className={styles.contentContainerHEW}>
-          <div className={styles.contentHeading}>
-            6) Should the device be in working condition to qualify for this
-            offer?
+          <div className={styles.contentContainerHEW}>
+            <div className={styles.contentHeading}>
+              9) In case I wish to return the new phone I bought with the
+              Exchange Offer, will my old phone be shipped back to me?
+            </div>
+            <div className={styles.contentDescriptionHEW}>
+              No. Unfortunately, the old phone you have handed over for exchange
+              cannot be shipped back to you. We will be refunded the value of
+              your new phone after deducting promotional cashback, if any.
+            </div>
           </div>
-          <div className={styles.contentDescriptionHEW}>
-            Yes. The phone needs to be in working condition. It also needs to
-            have all screen locks disabled and the iCloud Account unlocked to
-            enable pick-up by our partners.
+          <div className={styles.contentContainerHEW}>
+            <div className={styles.contentHeading}>
+              10) Can I hand in a different phone for Exchange?
+            </div>
+            <div className={styles.contentDescriptionHEW}>
+              No. You can only exchange the phone for which you have provided
+              details.
+            </div>
           </div>
-        </div>
-        <div className={styles.contentContainerHEW}>
-          <div className={styles.contentHeading}>
-            7) Can I exchange more than one device towards a purchase?
+          <div className={styles.contentContainerHEW}>
+            <div className={styles.contentHeading}>
+              11) Any pointers to keep in mind before I exchange my phone?
+            </div>
+            <div className={styles.contentDescriptionHEW}>
+              Kindly ensure all your data is backed up and cleared from your old
+              phone before handing over for exchange. You may also remove any
+              external memory card. TATACLiQ.comor the logistic partner will not
+              be responsible for any misuse of your data as a result. In the
+              event the device is not working, or the pick-up executive is not
+              able to do an iCloud unlock check, screen lock check and IMEI
+              check due to your phone being "factory reset" the exchange may not
+              go through.
+            </div>
           </div>
-          <div className={styles.contentDescriptionHEW}>
-            No. You may only exchange one device at a time.
-          </div>
-        </div>
-        <div className={styles.contentContainerHEW}>
-          <div className={styles.contentHeading}>
-            8) Can I provide one address for pick up and another for delivery?
-          </div>
-          <div className={styles.contentDescriptionHEW}>
-            No. The delivery address and pick-up address must be the same.
-          </div>
-        </div>
-        <div className={styles.contentContainerHEW}>
-          <div className={styles.contentHeading}>
-            9) In case I wish to return the new phone I bought with the Exchange
-            Offer, will my old phone be shipped back to me?
-          </div>
-          <div className={styles.contentDescriptionHEW}>
-            No. Unfortunately, the old phone you have handed over for exchange
-            cannot be shipped back to you. We will be refunded the value of your
-            new phone after deducting promotional cashback, if any.
-          </div>
-        </div>
-        <div className={styles.contentContainerHEW}>
-          <div className={styles.contentHeading}>
-            10) Can I hand in a different phone for Exchange?
-          </div>
-          <div className={styles.contentDescriptionHEW}>
-            No. You can only exchange the phone for which you have provided
-            details.
-          </div>
-        </div>
-        <div className={styles.contentContainerHEW}>
-          <div className={styles.contentHeading}>
-            11) Any pointers to keep in mind before I exchange my phone?
-          </div>
-          <div className={styles.contentDescriptionHEW}>
-            Kindly ensure all your data is backed up and cleared from your old
-            phone before handing over for exchange. You may also remove any
-            external memory card. TATACLiQ.comor the logistic partner will not
-            be responsible for any misuse of your data as a result. In the event
-            the device is not working, or the pick-up executive is not able to
-            do an iCloud unlock check, screen lock check and IMEI check due to
-            your phone being "factory reset" the exchange may not go through.
-          </div>
-        </div>
-        <div className={styles.contentContainerHEW}>
-          <div className={styles.contentHeading}>
-            12) Why do I see a pickup charge for availing the exchange offer?
-          </div>
-          <div className={styles.contentDescriptionHEW}>
-            When you avail the exchange offer, our courier partner picks up your
-            old device right from your doorstep. It is then shipped to the
-            seller in lieu of the new device. The pickup charge covers pickup
-            from your doorstep, packing and shipping of your old device.
+          <div className={styles.contentContainerHEW}>
+            <div className={styles.contentHeading}>
+              12) Why do I see a pickup charge for availing the exchange offer?
+            </div>
+            <div className={styles.contentDescriptionHEW}>
+              When you avail the exchange offer, our courier partner picks up
+              your old device right from your doorstep. It is then shipped to
+              the seller in lieu of the new device. The pickup charge covers
+              pickup from your doorstep, packing and shipping of your old
+              device.
+            </div>
           </div>
         </div>
       </div>
