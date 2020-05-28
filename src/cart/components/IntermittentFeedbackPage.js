a64385cc6a56898dea4af408be8fe090830fa160
deepak-tataunistore <dgupta@tataunistore.com>
2020-05-28 19:28:57 +0530

    changes for intermediate page api integration

--- src/cart/components/IntermittentFeedbackPage.js
+++ src/cart/components/IntermittentFeedbackPage.js
@@ -4,8 +4,8 @@ import styles from "./IntermittentFeedbackPage.css";
 import ProductImage from "../../general/components/ProductImage.js";
 import SecondaryLoader from "../../general/components/SecondaryLoader";
 
-const FORWARD_FLOW_URL = "feedback/NPSFeedbackForm?";
-const RETURN_FLOW_URL = "feedback/ReturnNPSFeedbackForm?";
+const FORWARD_FLOW_URL = "/feedback/NPSFeedbackForm?";
+const RETURN_FLOW_URL = "/feedback/ReturnNPSFeedbackForm?";
 class IntermittentFeedbackPage extends Component {
   constructor(props) {
     super(props);
@@ -15,12 +15,7 @@ class IntermittentFeedbackPage extends Component {
 
   componentDidMount() {
     if (this.props.getIntermittentPageData) {
-      let data = {
-        originalUid: "dgupta@tataunistore.com",
-        transactionId: "124192001717751",
-        deliveryMode: "home-delivery"
-      };
-      this.props.getIntermittentPageData(data);
+      this.props.getIntermittentPageData(this.getUserDetails);
     }
   }
 
@@ -42,7 +37,7 @@ class IntermittentFeedbackPage extends Component {
     this.props.history.push(URL);
   }
 
-  renderRatingData = rating => {
+  renderRatingData = (rating = []) => {
     let ratinglist = [];
 
     rating.forEach((r, index) => {
@@ -67,12 +62,15 @@ class IntermittentFeedbackPage extends Component {
       return <SecondaryLoader />;
     }
 
-    let {
-      product: { imageURL, productName, deliveryDate },
-      ratings,
-      subTitle,
-      title
-    } = feedbackData.data; //this.props.feedbackData.data;
+    if (this.props && this.props.feedBackPageData) {
+      var {
+        product: { imageURL, productName, deliveryDate },
+        ratings,
+        subTitle,
+        title
+      } =
+        this.props.feedBackPageData && this.props.feedBackPageData.data;
+    }
 
     return (
       <div className={styles.base}>
@@ -131,74 +129,3 @@ class IntermittentFeedbackPage extends Component {
 }
 
 export default IntermittentFeedbackPage;
-
-const feedbackData = {
-  status: "success",
-  errorCode: "NPS001",
-  message: "NPS Rating successfull",
-  data: {
-    title: "Time to know, did we CLiQ?",
-    subTitle: "How likely are you going to recommend us",
-    ratings: [
-      {
-        displayName: "Not Likely",
-        ratingList: [1, 2, 3, 4, 5, 6],
-        hexCode: "#ff3435"
-      },
-      {
-        displayName: "Maybe",
-        ratingList: [7, 8],
-        hexCode: "#ff3456"
-      },
-      {
-        displayName: "Likely",
-        ratingList: [9, 10],
-        hexCode: "#ff3489"
-      }
-    ],
-    product: {
-      USSID: "10035032GVPL",
-      categoryHierarchy: [
-        {
-          category_id: "BSH1E00193",
-          category_name: "VU"
-        },
-        {
-          category_id: "BSH1E00193MSH12",
-          category_name: "Electronics"
-        },
-        {
-          category_id: "BSH1E00193MSH1216",
-          category_name: "TV"
-        },
-        {
-          category_id: "BSH1E00193MSH1216105",
-          category_name: "HD Ready TV"
-        }
-      ],
-      consignmentStatus: "DELIVERED",
-      deliveryDate: "Dec 30 2019",
-      imageURL:
-        "//img.tatacliq.com/images/i5/97Wx144H/MP000000005718665_97Wx144H_20191010152837.jpeg",
-      logisticName: "Blue Dart",
-      price: "7879.68",
-      totalDiscount: "1240.00",
-      productBrand: "VU",
-      productName:
-        "VU 80 cm (32 Inches) HD Ready LED TV 32GVPL (2019 Model, Black)",
-      productcode: "MP000000005718665",
-      returnPolicy: "7",
-      selectedDeliveryMode: {
-        code: "home-delivery",
-        deliveryCost: "0.0",
-        desc: "Delivered in 2-5 days",
-        name: "Home Delivery"
-      },
-      sellerID: "100350",
-      sellerName: "DIGITAL STORE",
-      sellerorderno: "191226-006-635781",
-      serialno: "32GVPL19H009792",
-      statusDisplay: "Delivered"
-    }
-  }
-};
