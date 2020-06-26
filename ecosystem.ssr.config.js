3421772075b2996f5ee1ac2efd0f2b84df642574
Shriney Malekar <smalekar@tataunistore.com>
2020-06-26 23:11:41 +0530

    Branch name added in config file

--- ecosystem.ssr.config.js
+++ ecosystem.ssr.config.js
@@ -14,7 +14,7 @@ module.exports = {
       user: "ubuntu",
       host: "54.147.12.99",
       key: "~/.ssh/ORACLE-HYBRIS.pem",
-      ref: "origin/desktopReleaseHotFix18062020",
+      ref: "origin/UPF-208-logout-error-desktop",
       repo: "git@github.com:tcs-chennai/TUL_PWA_FRONT_NEW_END_CODE.git",
       ssh_options: ["StrictHostKeyChecking=no", "PasswordAuthentication=no"],
       path: "/home/ubuntu/tata-cliq-frontend",
