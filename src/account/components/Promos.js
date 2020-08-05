fe69f68cd9376f1b034db5c396ba05a25dd77dc2
chandan7193 <chandan.singh7193@gmail.com>
2020-08-05 20:33:40 +0530

    Tiles issue fixed

--- src/account/components/Promos.js
+++ src/account/components/Promos.js
@@ -1,41 +1,44 @@
 import React, { Component } from "react";
 import styles from "./Promos.css";
 // import Received from "./img/utilized.svg";
-import ExpiredImg from "./img/expired.svg";
-import ExpiringImg from "./img/expiring.svg";
+import Expired from "./img/expired.svg";
+import Expiring from "./img/expiring.svg";
 import Paid from "./img/utilized.svg";
 import format from "date-fns/format";
 import PropTypes from "prop-types";
-import { RECEIVED, PAID, EXPIRED, Expiring } from "../../lib/constants";
+import { RECEIVED, PAID, EXPIRED } from "../../lib/constants";
 const dateFormat = "DD MMM YYYY";
 
 export default class Promos extends Component {
-  promo = (promoType, date, expiryDate, transactionStatus) => {
-    if (promoType === "Expired" || transactionStatus == "CANCELLED") {
-      return <img src={ExpiredImg} className={styles.rewardsTypeImg} />;
-    } else if (promoType === "Paid") {
+  promo = item => {
+    let transactionType = item.transactionType;
+    if (transactionType == "Expired" || transactionType == "CANCELLED") {
+      return <img src={Expired} className={styles.rewardsTypeImg} />;
+    } else if (transactionType == "Paid") {
       return <img src={Paid} className={styles.rewardsTypeImg} />;
-    } else if (promoType === Expiring) {
-      return <img src={ExpiringImg} className={styles.rewardsTypeImg} />;
-    } else if (promoType === RECEIVED && expiryDate < date) {
-      return <img src={ExpiringImg} className={styles.rewardsTypeImg} />;
+    } else if (transactionType == "Expiring") {
+      return <img src={Expiring} className={styles.rewardsTypeImg} />;
+    } else if (
+      transactionType == "Received" ||
+      transactionType == "RECEIVED REFUND" ||
+      transactionType == "ADDED"
+    ) {
+      return <img src={Expiring} className={styles.rewardsTypeImg} />;
     } else {
       return null;
     }
   };
   promoText = item => {
     let today = new Date().getTime();
-
     let expiringDate = new Date(item.expiryDate).getTime();
-
     let transactionType = item.transactionType;
     let transactionStatus = item.transactionStatus;
-
+    let transactionDateFormatted = format(item.transactionDate, dateFormat);
     let time = format(item.transactionDate, dateFormat);
     let formattedExpriry = format(expiringDate, dateFormat);
     if (item.transactionType === "Paid") {
       return <p className={styles.rewardExpDate}>Utilised: {time}</p>;
-    } else if (item.transactionType === "Expired" || expiringDate < today) {
+    } else if (item.transactionType === "Expired") {
       return <p className={styles.rewardExpDate}>Expired on: {time}</p>;
     } else if (item.transactionType === "Expiring") {
       return <p className={styles.rewardExpDate}>Expiring on: {time}</p>;
@@ -43,7 +46,13 @@ export default class Promos extends Component {
       return (
         <p className={styles.rewardExpDate}>Expiring on: {formattedExpriry}</p>
       );
-    } else if (transactionStatus == "CANCELLED" && expiringDate < 0) {
+    } else if (transactionType == "Received" && expiringDate < today) {
+      return (
+        <p className={styles.rewardExpDate}>
+          Recieved on: {transactionDateFormatted}
+        </p>
+      );
+    } else if (transactionStatus == "CANCELLED") {
       return <p className={styles.rewardExpDate}> Cancelled: {time}</p>;
     } else return null;
   };
@@ -55,7 +64,10 @@ export default class Promos extends Component {
     let transactionStatus = this.props.item.transactionStatus;
 
     let redeemStartDate = Date.parse(this.props.item.redeemStartDate);
-
+    let formattedRedeemStartDate = format(
+      this.props.item.redeemStartDate,
+      dateFormat
+    );
     let date = new Date().getTime();
 
     return (
@@ -63,7 +75,7 @@ export default class Promos extends Component {
         {" "}
         {promotype !== RECEIVED && (
           <div className={styles.promoContainer}>
-            {this.promo(promotype, date, redeemStartDate, transactionStatus)}
+            {this.promo(this.props.item)}
             <p className={styles.rewardAmt}>{amount}</p>
             {this.promoText(this.props.item)}
           </div>
@@ -73,17 +85,20 @@ export default class Promos extends Component {
             <div className={styles.availFromBlock}>
               <div className={styles.availTxtBlock}>
                 <p className={styles.availFromtxt}>Available from</p>
-                <p className={styles.availFromDate}>{redeemStartDate}</p>
+                <p className={styles.availFromDate}>
+                  {formattedRedeemStartDate}
+                </p>
               </div>
             </div>
           )}
-        {promotype == "Received" && (
-          <div className={styles.promoContainer}>
-            {this.promo(promotype, date, redeemStartDate, transactionStatus)}
-            <p className={styles.rewardAmt}>{amount}</p>
-            {this.promoText(this.props.item)}
-          </div>
-        )}
+        {promotype == RECEIVED &&
+          redeemStartDate < date && (
+            <div className={styles.promoContainer}>
+              {this.promo(this.props.item)}
+              <p className={styles.rewardAmt}>{amount}</p>
+              {this.promoText(this.props.item)}
+            </div>
+          )}
       </React.Fragment>
     );
   }
