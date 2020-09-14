b7bf3d9e0b90d0fb33137fefd5df4c4266c2d948
Sandeep Vemula <sandeepvemula@Admins-MacBook-Pro.local>
2020-09-14 20:16:13 +0530

    CSS changes

--- src/pdp/components/PdpBeautyDesktop/ImageGalleryContentComponents/ProductDetailsSection/OffersComponent.js
+++ src/pdp/components/PdpBeautyDesktop/ImageGalleryContentComponents/ProductDetailsSection/OffersComponent.js
@@ -55,7 +55,7 @@ export default class OffersComponent extends React.Component {
                   onClick={e => this.showVoucherOffersModal(e, offers)}
                   className={styles["offers-list-link"]}
                 >
-                  More
+                  See all
                 </a>
               )}
             </div>
