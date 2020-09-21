45c53d80f6a2f9a0adf5eb8a32d270e5e7dd9510
Siddhesh Rane <srane@tataunistore.com>
2020-09-21 15:13:34 +0530

    MDEQ-744

--- src/cart/components/CartItemForDesktop.js
+++ src/cart/components/CartItemForDesktop.js
@@ -55,11 +55,11 @@ export default class CartItemForDesktop extends React.Component {
     document.title = "Shopping Cart - TATA CLiQ";
     if (
       this.props.product &&
-      this.props.product.exchangeDetails &&
-      this.props.product.exchangeDetails.exchangeCancelMessage
+      this.props.product.exchangeInfo &&
+      this.props.product.exchangeInfo.exchangeCancelMessage
     ) {
       this.props.displayToast(
-        this.props.product.exchangeDetails.exchangeCancelMessage
+        this.props.product.exchangeInfo.exchangeCancelMessage
       );
     }
   }
