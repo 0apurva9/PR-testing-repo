186914139e169b48326f2c1ef5749085e8c94f49
chandan7193 <csingh@tataunistore.com>
2020-09-22 15:34:57 +0530

    qa6 pointing to preprod3

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -170,9 +170,9 @@ if (
   API_URL_ROOT = "https://mock.tatacliq.com/marketplacewebservices";
   MIDDLEWARE_API_URL_ROOT = "https://mock.tatacliq.com/marketplacewebservices";
 } else if (process.env.REACT_APP_STAGE === "qa6") {
-  API_URL_ROOT = "https://qa6.tataunistore.com/marketplacewebservices";
+  API_URL_ROOT = "https://preprod3.tataunistore.com/marketplacewebservices";
   MIDDLEWARE_API_URL_ROOT =
-    "https://qa6.tataunistore.com/marketplacewebservices";
+    "https://preprod3.tataunistore.com/marketplacewebservices";
 }
 
 if (process.env.REACT_APP_STAGE === "tmpprod") {
