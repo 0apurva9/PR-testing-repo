ec113369264bd33849ad8de04f4b09aa6b949760
vinayak-dew <vinayakj@dewsolutions>
2020-12-02 20:45:01 +0530

    UPF-1857: Add login for already Linked Mobile Number

--- src/mobile-number-login/store/mobile-number-login.actions.ts
+++ src/mobile-number-login/store/mobile-number-login.actions.ts
@@ -182,6 +182,13 @@ export function generateOTP() {
 export function validateOtp() {
     return async (dispatch: Function, getState: () => RootState, { api }: { api: any }) => {
         const apiData = getState().mobileNumberLogin.mnlApiData;
+        const mnlApiResponseState = getState().mobileNumberLogin.mnlApiResponse;
+        if (mnlApiResponseState && !mnlApiResponseState.userData.customer.numberAdded) {
+            apiData.pass = "";
+        }
+        if (mnlApiResponseState && mnlApiResponseState.userData.validation && mnlApiResponseState.userData.validation.changedmailId) {
+            apiData.email = mnlApiResponseState.userData.validation.changedmailId;
+        }
         let globalAccessToken = await getFetchGlobalAccessToken(dispatch);
 
         const result: Response = await api.post("mobileloginapi/v1/authnuser/authenticate", apiData, true, {
