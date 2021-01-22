388b1be1f9b8960046310004bfe3c9c666e74361
chandan7193 <csingh@tataunistore.com>
2021-01-22 12:29:12 +0530

    Validate

--- src/cart/actions/cart.actions.js
+++ src/cart/actions/cart.actions.js
@@ -60,7 +60,8 @@ import {
   EMI_BANK_TERMS_AND_CONDITIONS,
   INVALID_BANK_COUPON_POPUP,
   VALIDATE_OFFERS_POPUP,
-  NON_EMI_ELIGIBLE_TO_WISHLIST
+  NON_EMI_ELIGIBLE_TO_WISHLIST,
+  VALIDATE_CLIQ_CASH_POPUP
 } from "../../general/modal.actions";
 import { displayToast } from "../../general/toast.actions";
 import { setUrlToRedirectToAfterAuth } from "../../auth/actions/auth.actions.js";
