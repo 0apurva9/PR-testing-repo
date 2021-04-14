0406e0c475e5498c2b7237bcada91669c02af6ed
Pulkit <81670609+pulkitdev@users.noreply.github.com>
2021-04-14 11:47:52 +0530

    Temp dynamic url (#1222)
    Co-authored-by: ramekabal <ramekabels@dewsolutions.in>
    Co-authored-by: santosh-dew <40351678+santosh-dew@users.noreply.github.com>
    Co-authored-by: Ramekabal <58032721+Ramekabal@users.noreply.github.com>

--- src/return/components/ReturnFlowDesktop.tsx
+++ src/return/components/ReturnFlowDesktop.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable  @typescript-eslint/no-explicit-any */
 import * as React from "react";
 import { Route } from "react-router-dom";
 import {
