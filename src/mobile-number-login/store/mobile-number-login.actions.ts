3dd766f2cb207073b949f7f9c013b97cb62292da
singlaromaldew <romal@dewsolutions.in>
2021-04-27 17:23:07 +0530

    changes

--- src/mobile-number-login/store/mobile-number-login.actions.ts
+++ src/mobile-number-login/store/mobile-number-login.actions.ts
@@ -725,7 +725,9 @@ export function updatePasswordProfile() {
         if (errorStatus.status) {
             dispatch(hideSecondaryLoader());
             if (errorStatus.errorcode === "406") {
-                await dispatch(setPasswordErrorMsg(errorStatus.message));
+                await dispatch(displayToast(errorStatus.message));
+                dispatch(hideMobileNumberLoginModal());
+                // await dispatch(setPasswordErrorMsg(errorStatus.message));
             } else if (errorStatus.errorcode === "4011") {
                 await dispatch(displayToast(errorStatus.message));
                 dispatch(hideMobileNumberLoginModal());
