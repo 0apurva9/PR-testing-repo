535e3a32409da4fd995836e1dc05230f403a0cf8
santosh-dew <40351678+santosh-dew@users.noreply.github.com>
2021-05-05 12:36:26 +0530

    Code update for removing base url (#1361)

--- src/common/services/common.services.js
+++ src/common/services/common.services.js
@@ -1,64 +1,66 @@
-import {
-  CUSTOMER_ACCESS_TOKEN,
-  LOGGED_IN_USER_DETAILS
-} from "./../../lib/constants";
+import { CUSTOMER_ACCESS_TOKEN, LOGGED_IN_USER_DETAILS, NOT_FOUND } from "./../../lib/constants";
 import * as Cookies from "./../../lib/Cookie";
 
-import {
-  refreshCustomerAccessToken,
-  logoutUserOnInvalidRefreshToken
-} from "./../../lib/apiRequest";
+import { refreshCustomerAccessToken, logoutUserOnInvalidRefreshToken } from "./../../lib/apiRequest";
 
 export async function getCustomerAccessToken() {
-  let customerCookie = Cookies.getCookie(CUSTOMER_ACCESS_TOKEN);
-  if (!JSON.parse(customerCookie).access_token) {
-    try {
-      let refreshCustomerToken = await refreshCustomerAccessToken();
-      if (!refreshCustomerToken) {
-        let userDetails = Cookies.getCookie(LOGGED_IN_USER_DETAILS);
-        if (userDetails) {
-          logoutUserOnInvalidRefreshToken();
-        }
-      } else {
-        Cookies.deleteCookie(CUSTOMER_ACCESS_TOKEN);
-        Cookies.createCookie(
-          CUSTOMER_ACCESS_TOKEN,
-          JSON.stringify(refreshCustomerToken)
-        );
+    let customerCookie = Cookies.getCookie(CUSTOMER_ACCESS_TOKEN);
+    if (!JSON.parse(customerCookie).access_token) {
+        try {
+            let refreshCustomerToken = await refreshCustomerAccessToken();
+            if (!refreshCustomerToken) {
+                let userDetails = Cookies.getCookie(LOGGED_IN_USER_DETAILS);
+                if (userDetails) {
+                    logoutUserOnInvalidRefreshToken();
+                }
+            } else {
+                Cookies.deleteCookie(CUSTOMER_ACCESS_TOKEN);
+                Cookies.createCookie(CUSTOMER_ACCESS_TOKEN, JSON.stringify(refreshCustomerToken));
 
-        return JSON.parse(Cookies.getCookie(CUSTOMER_ACCESS_TOKEN))
-          .access_token;
-      }
-    } catch (error) {
-      let userDetails = Cookies.getCookie(LOGGED_IN_USER_DETAILS);
-      if (userDetails) {
-        logoutUserOnInvalidRefreshToken();
-      }
+                return JSON.parse(Cookies.getCookie(CUSTOMER_ACCESS_TOKEN)).access_token;
+            }
+        } catch (error) {
+            let userDetails = Cookies.getCookie(LOGGED_IN_USER_DETAILS);
+            if (userDetails) {
+                logoutUserOnInvalidRefreshToken();
+            }
+        }
+    } else {
+        return JSON.parse(customerCookie).access_token;
     }
-  } else {
-    return JSON.parse(customerCookie).access_token;
-  }
 }
 
 export function getLoggedInUserDetails() {
-  const loggedInUserDetailsCookie = Cookies.getCookie(LOGGED_IN_USER_DETAILS);
-  const loggedInUserDetails =
-    loggedInUserDetailsCookie && JSON.parse(loggedInUserDetailsCookie);
-  return loggedInUserDetails;
+    const loggedInUserDetailsCookie = Cookies.getCookie(LOGGED_IN_USER_DETAILS);
+    const loggedInUserDetails = loggedInUserDetailsCookie && JSON.parse(loggedInUserDetailsCookie);
+    return loggedInUserDetails;
 }
 
 export function initiateHaptikScript() {
-  const f = document.getElementsByTagName("SCRIPT")[0];
-  const p = document.createElement("SCRIPT");
-  const timestamp = new Date().getTime();
-  const source_url = process.env.HAPTIK_CHATBOT_URL + "/static/aspectwise/js/haptik.js?" + timestamp;
-  p.type = "text/javascript";
-  p.setAttribute("charset", "utf-8");
-  p.setAttribute("clientid", "tatacliq");
-  p.async = true;
-  p.id = "buzzosrc";
-  p.src = source_url;
-  if (!document.getElementById("buzzosrc")) {
-      f.parentNode.insertBefore(p, f);
-  }
+    const f = document.getElementsByTagName("SCRIPT")[0];
+    const p = document.createElement("SCRIPT");
+    const timestamp = new Date().getTime();
+    const source_url = process.env.HAPTIK_CHATBOT_URL + "/static/aspectwise/js/haptik.js?" + timestamp;
+    p.type = "text/javascript";
+    p.setAttribute("charset", "utf-8");
+    p.setAttribute("clientid", "tatacliq");
+    p.async = true;
+    p.id = "buzzosrc";
+    p.src = source_url;
+    if (!document.getElementById("buzzosrc")) {
+        f.parentNode.insertBefore(p, f);
+    }
+}
+
+export function doRemoveBaseUrl(url) {
+    if (!url) {
+        return NOT_FOUND;
+    }
+
+    try {
+        const location = new URL(url);
+        return `${location.pathname.toLowerCase()}`;
+    } catch (error) {
+        return url.toLowerCase();
+    }
 }
